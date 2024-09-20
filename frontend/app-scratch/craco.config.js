c49b4325beb6e49f997d836eff9468bea0e6013e
Nico Hauser <nico@anansi-solutions.net>
2024-09-20 17:29:13 +0200

    Make CSS modules work for scratch

--- frontend/app-scratch/craco.config.js
+++ frontend/app-scratch/craco.config.js
@@ -1,100 +1,112 @@
 /* eslint-disable no-undef */
 /* craco.config.js */
 const path = require(`path`);
-const webpack = require("webpack");
+const webpack = require(`webpack`);
+
+// craco plugins
 const cracoBabelLoader = require("craco-babel-loader");
+const cracoCSSModules = require("craco-css-modules");
 
-// Plugins
+// webpack plugins
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-
-const ScratchWebpackConfigBuilder = require("scratch-webpack-configuration");
-
-const baseConfig = new ScratchWebpackConfigBuilder({
-  rootPath: path.resolve(__dirname),
-  enableReact: true,
-  shouldSplitChunks: false,
-})
-  .setTarget("browserslist")
-  .merge({
-    output: {
-      assetModuleFilename: "static/assets/[name].[hash][ext][query]",
-      library: {
-        name: "GUI",
-        type: "umd2",
-      },
-    },
-    resolve: {
-      fallback: {
-        Buffer: require.resolve("buffer/"),
-        stream: require.resolve("stream-browserify"),
-      },
-    },
-  })
-  .addModuleRule({
-    test: /\.(svg|png|wav|mp3|gif|jpg)$/,
-    resourceQuery: /^$/, // reject any query string
-    type: "asset", // let webpack decide on the best type of asset
-  })
-  .addPlugin(
-    new webpack.DefinePlugin({
-      "process.env.DEBUG": Boolean(process.env.DEBUG),
-      "process.env.GA_ID": `"${process.env.GA_ID || "UA-000000-01"}"`,
-      "process.env.GTM_ENV_AUTH": `"${process.env.GTM_ENV_AUTH || ""}"`,
-      "process.env.GTM_ID": process.env.GTM_ID
-        ? `"${process.env.GTM_ID}"`
-        : null,
-    }),
-  )
-  .addPlugin(
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "node_modules/scratch-blocks/media",
-          to: "static/blocks-media/default",
-        },
-        {
-          from: "node_modules/scratch-blocks/media",
-          to: "static/blocks-media/high-contrast",
-        },
-        {
-          // overwrite some of the default block media with high-contrast versions
-          // this entry must come after copying scratch-blocks/media into the high-contrast directory
-          from: "src/scratch/scratch-gui/src/lib/themes/high-contrast/blocks-media",
-          to: "static/blocks-media/high-contrast",
-          force: true,
-        },
-        {
-          context: "node_modules/scratch-vm/dist/web",
-          from: "extension-worker.{js,js.map}",
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-  );
-
-const config = baseConfig.get();
-
-if (!process.env.CI) {
-  baseConfig.addPlugin(new webpack.ProgressPlugin());
-}
 
 const resolvePath = (relativePath) => path.resolve(__dirname, relativePath);
 
 module.exports = {
+  // see https://craco.js.org/docs/configuration/webpack/
   webpack: {
-    ...config,
     alias: {
       "@scratch-submodule": resolvePath("src/scratch"),
     },
-    // for craco this must be put here
-    // https://stackoverflow.com/a/76150844/2897827
-    configure: {
+    // webpack plugins
+    plugins: {
+      add: [
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+        }),
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: "node_modules/scratch-blocks/media",
+              to: "src/scratch/scratch-gui/static/blocks-media/default",
+            },
+            {
+              from: "node_modules/scratch-blocks/media",
+              to: "src/scratch/scratch-gui/static/blocks-media/high-contrast",
+            },
+            {
+              // overwrite some of the default block media with high-contrast versions
+              // this entry must come after copying scratch-blocks/media into the high-contrast directory
+              from: "src/scratch/scratch-gui/src/lib/themes/high-contrast/blocks-media",
+              to: "src/scratch/scratch-gui/static/blocks-media/high-contrast",
+              force: true,
+            },
+            {
+              context: "node_modules/scratch-vm/dist/web",
+              from: "extension-worker.{js,js.map}",
+              noErrorOnMissing: true,
+            },
+          ],
+        }),
+      ],
+      remove: [],
+    },
+    /*style: {
+      css: {
+        loaderOptions: {
+          modules: {
+            auto: true,
+            exportLocalsConvention: function (name) {
+              console.log("name", name);
+              return "test123";
+            },
+          },
+        },
+      },
+    },*/
+    // https://craco.js.org/docs/configuration/getting-started/#object-literals-and-functions
+    configure: (webpackConfig) => {
       // supress invalid source map warnings - some dependencies have invalid or inexistent source maps
       // https://stackoverflow.com/a/70975849/2897827
       // https://github.com/facebook/create-react-app/pull/11752
-      ignoreWarnings: [/Failed to parse source map/],
+      webpackConfig.ignoreWarnings = [/Failed to parse source map/];
+
+      webpackConfig.resolve.fallback = {
+        // provide web-compatible fallbacks for node modules, re
+        ...webpackConfig.resolve.fallback,
+        // The '/' is important, see https://www.npmjs.com/package/buffer#usage
+        buffer: require.resolve("buffer/"),
+        stream: require.resolve("stream-browserify"),
+      };
+
+      // kebab-case to camelCase for CSS class names in CSS modules - this is required by scratch
+
+      // copied and adapted from https://stackoverflow.com/a/74149013/2897827
+
+      webpackConfig.module.rules
+        .find(({ oneOf }) => !!oneOf)
+        .oneOf.filter(({ use }) => JSON.stringify(use)?.includes("css-loader"))
+        .reduce((acc, { use }) => acc.concat(use), [])
+        .filter(
+          (use) =>
+            typeof use === "object" &&
+            use.loader &&
+            use.loader.includes("css-loader"),
+        )
+        .forEach(({ options }) => {
+          if (options.modules) {
+            // following the scratch config at https://github.com/scratchfoundation/scratch-webpack-configuration/blob/06b35c21a82ff29a596bd7a4c233731a00d3d49e/src/index.cjs#L173
+            options.modules.exportLocalsConvention = "camelCase";
+
+            // enable CSS modules for all files, see https://webpack.js.org/loaders/css-loader/#auto
+            options.modules.auto = undefined;
+          }
+        });
+
+      return webpackConfig;
     },
   },
+  // craco plugins
   plugins: [
     {
       plugin: cracoBabelLoader,
@@ -102,5 +114,6 @@ module.exports = {
         includes: [resolvePath("node_modules/scratch-paint")],
       },
     },
+    { plugin: cracoCSSModules },
   ],
 };
