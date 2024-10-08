e44a7bdd6dbed289b484633c75109c0f2fef238a
Nico Hauser <nico@anansi-solutions.net>
2024-10-08 15:30:34 +0200

    Try to get sonar running

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./services/users.service";
+import { CoreModule } from "../core/core.module";
 
 describe("UsersController", () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CoreModule],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
