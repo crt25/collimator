82376e0f0c3a1405c8abac97f1cb7a87bb81964a
Nico Hauser <nico@anansi-solutions.net>
2024-10-14 10:12:26 +0200

    [CRT-2] Update linter config

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,18 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   extends: [
-    path.join(__dirname, '../.eslintrc.js'),
-    "plugin:prettier/recommended"
+    path.join(__dirname, "../.eslintrc.js"),
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module",
   },
   env: {
     node: true,
     jest: true,
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
   },
 };
