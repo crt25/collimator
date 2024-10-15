3e8c06fe25069ada945b8795a62e69cd8138ba2a
Nico Hauser <nico@anansi-solutions.net>
2024-10-15 10:36:14 +0200

    [CRT-6] Add tests for remaining analyzers

---
+++ backend/src/data-analyzer/criteria-based-analyzers/__tests__/contains-condition.spec.ts
@@ -0,0 +1,111 @@
+import { AstNodeType, GeneralAst } from "src/ast/types/general-ast";
+import { ExpressionNodeType } from "src/ast/types/general-ast/ast-nodes/expression-node";
+import { StatementNodeType } from "src/ast/types/general-ast/ast-nodes";
+import { containsCondition } from "../contains-condition";
+
+describe("Criteria Based Analyzer", () => {
+  describe("containsCondition", () => {
+    it("returns true if the AST contains a condition", () => {
+      const output = containsCondition(
+        [
+          {
+            nodeType: AstNodeType.actor,
+            eventListeners: [
+              {
+                nodeType: AstNodeType.eventListener,
+                condition: {
+                  event: "event_whengreaterthan",
+                  parameters: [
+                    {
+                      nodeType: AstNodeType.expression,
+                      expressionType: ExpressionNodeType.literal,
+                      type: "number",
+                      value: "10",
+                    },
+                    {
+                      nodeType: AstNodeType.expression,
+                      expressionType: ExpressionNodeType.literal,
+                      type: "string",
+                      value: "LOUDNESS",
+                    },
+                  ],
+                },
+                action: {
+                  nodeType: AstNodeType.statement,
+                  statementType: StatementNodeType.sequence,
+                  statements: [
+                    {
+                      nodeType: AstNodeType.statement,
+                      statementType: StatementNodeType.condition,
+                      condition: {
+                        nodeType: AstNodeType.expression,
+                        expressionType: ExpressionNodeType.literal,
+                        type: "boolean",
+                        value: "true",
+                      },
+                      whenTrue: {
+                        nodeType: AstNodeType.statement,
+                        statementType: StatementNodeType.sequence,
+                        statements: [],
+                      },
+                      whenFalse: {
+                        nodeType: AstNodeType.statement,
+                        statementType: StatementNodeType.sequence,
+                        statements: [],
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+            functionDeclarations: [],
+          },
+        ] as GeneralAst,
+        true,
+      );
+
+      expect(output).toBe(true);
+    });
+
+    it("returns false if the AST does not contain a condition", () => {
+      const output = containsCondition(
+        [
+          {
+            nodeType: AstNodeType.actor,
+            eventListeners: [
+              {
+                nodeType: AstNodeType.eventListener,
+                condition: {
+                  event: "event_whengreaterthan",
+                  parameters: [
+                    {
+                      nodeType: AstNodeType.expression,
+                      expressionType: ExpressionNodeType.literal,
+                      type: "number",
+                      value: "10",
+                    },
+                    {
+                      nodeType: AstNodeType.expression,
+                      expressionType: ExpressionNodeType.functionCall,
+                      name: "myFunction",
+                      arguments: [],
+                    },
+                  ],
+                },
+                action: {
+                  nodeType: AstNodeType.statement,
+                  statementType: StatementNodeType.sequence,
+                  statements: [],
+                },
+              },
+            ],
+            functionDeclarations: [],
+          },
+        ] as GeneralAst,
+        true,
+      );
+
+      expect(output).toBe(false);
+    });
+  });
+});
