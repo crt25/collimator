9b426c39ec3cf3be2a846d9c74f5d9cd7f3d6636
Nico Hauser <nico@anansi-solutions.net>
2024-10-25 20:13:00 +0200

    [CRT-20] [CRT-21] Fix linting issues

--- frontend/src/__tests__/typing.ts
+++ frontend/src/__tests__/typing.ts
@@ -1,6 +1,4 @@
-import type {  ReactRenderer } from "@storybook/react";
+import type { ReactRenderer } from "@storybook/react";
 import { DecoratorFunction } from "storybook/internal/types";
 
-
-
-export type StorybookDecorator = DecoratorFunction<ReactRenderer>
+export type StorybookDecorator = DecoratorFunction<ReactRenderer>;
