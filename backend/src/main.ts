4391f9d55c9ef9cab5cd3780f957b340f59e1583
Nico Hauser <nico@anansi-solutions.net>
2024-11-05 15:32:10 +0100

    [CRT-3] Enable CORS on the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,22 @@ async function bootstrap(): Promise<void> {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
+
+  // enable CORS for the frontend
+  const frontendHostname = app
+    .get(ConfigService)
+    .get<string>("FRONTEND_HOSTNAME");
+
+  app.enableCors({
+    origin: frontendHostname,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: [],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    // ignore preflight requests
+    preflightContinue: false,
+    // we're not using cookies, so we don't need to send credentials
+    credentials: false,
+  });
 
   await app.listen(3000);
 }
