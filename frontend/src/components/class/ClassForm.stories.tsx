3208268d91a9d4a72c236713ddb935bc940641e0
Nico Hauser <nico@anansi-solutions.net>
2024-11-06 18:21:23 +0100

    [CRT-3] Fix CI

--- frontend/src/components/class/ClassForm.stories.tsx
+++ frontend/src/components/class/ClassForm.stories.tsx
@@ -1,11 +1,26 @@
 import { fn } from "@storybook/test";
 import ClassForm from "./ClassForm";
+import { getUsersControllerFindAllUrl } from "@/api/collimator/generated/endpoints/users/users";
+import { backendHostName } from "@/utilities/constants";
+import { getUsersControllerFindAllResponseMock } from "@/api/collimator/generated/endpoints/users/users.msw";
 
 type Args = Parameters<typeof ClassForm>[0];
+
+const users = getUsersControllerFindAllResponseMock();
 
 export default {
   component: ClassForm,
   title: "ClassForm",
+  parameters: {
+    mockData: [
+      {
+        url: `${backendHostName}${getUsersControllerFindAllUrl()}`,
+        method: "GET",
+        status: 200,
+        response: users,
+      },
+    ],
+  },
 };
 
 export const Default = {
@@ -37,6 +52,7 @@ export const WithInitialValues = {
     onSubmit: fn(),
     initialValues: {
       name: "Class 1",
+      teacherId: users[0].id,
     },
   } as Args,
 };
