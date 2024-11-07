83dd6d3a5afc22af91f4fbc256bee5aae6bcdd87
Nico Hauser <nico@anansi-solutions.net>
2024-11-07 15:20:42 +0100

    [CRT-5] Generate task api calls

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -3,9 +3,10 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 import * as interceptors from "./interceptors";
 import { UsersModule } from "./users/users.module";
 import { ClassesModule } from "./classes/classes.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @Module({
-  imports: [UsersModule, ClassesModule],
+  imports: [UsersModule, ClassesModule, TasksModule],
   providers: [
     {
       provide: APP_INTERCEPTOR,
