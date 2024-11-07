4b8173af29e20920db99354f69257ba5ce411756
Pierluca Borsò <pierluca@anansi-solutions.net>
2024-11-07 12:21:53 +0100

    [CRT-5] Added test to verify spurious fields are ignored recursively in DTOs

--- backend/src/api/classes/dto/existing-class-with-teacher.dto.spec.ts
+++ backend/src/api/classes/dto/existing-class-with-teacher.dto.spec.ts
@@ -1,11 +1,13 @@
 import { ExistingClassWithTeacherDto } from "./existing-class-with-teacher.dto";
+import { ClassTeacherDto } from "./class-teacher.dto";
 
 describe("ExistingClassWithTeacherDto", () => {
+  const teacher = { id: 5, name: "Jerry Smith" };
   const klass = {
     id: 1,
     name: "Test Class",
     teacherId: 5,
-    teacher: { id: 5, name: "Jerry Smith" },
+    teacher: { ...teacher, spuriousField: 3 },
   };
 
   it("can be constructed", () => {
@@ -13,7 +15,8 @@ describe("ExistingClassWithTeacherDto", () => {
 
     expect(classDto.id).toEqual(klass.id);
     expect(classDto.name).toEqual(klass.name);
-    expect(classDto.teacher).toEqual(klass.teacher);
     expect(classDto).not.toHaveProperty("teacherId");
+    expect(classDto.teacher).toBeInstanceOf(ClassTeacherDto);
+    expect(classDto.teacher).toEqual(teacher);
   });
 });
