9c4d417c414d236862530dc9fb84a94a3d70913f
Nico Hauser <nico@anansi-solutions.net>
2025-01-10 10:50:26 +0100

    [CRT-43] Address code review comments

--- e2e/scripts/build-frontend.ts
+++ e2e/scripts/build-frontend.ts
@@ -2,16 +2,12 @@ import { buildFrontend } from "../setup/helpers";
 import { mockOidcClientId, mockOidcProxyUrl } from "../setup/config";
 
 const main = async (): Promise<void> => {
-  buildFrontend(
-    {
-      // use a relative path s.t. we don't have to rebuild the frontend for each test
-      backendHostname: "",
-      oidcUrl: mockOidcProxyUrl,
-      oidcClientId: mockOidcClientId,
-    },
-    "pipe",
-    "pipe",
-  );
+  buildFrontend({
+    // use a relative path s.t. we don't have to rebuild the frontend for each test
+    backendHostname: "",
+    oidcUrl: mockOidcProxyUrl,
+    oidcClientId: mockOidcClientId,
+  });
 };
 
 main().catch((error) => {
