b90929f760dfa15bf44de094c39655404da6c367
Nico Hauser <nico@anansi-solutions.net>
2025-02-10 11:26:24 +0100

    Create new module for bootstrapping S3 state

---
+++ terraform/remote-state/main.tf
@@ -0,0 +1,31 @@
+﻿resource "aws_s3_bucket" "this" {
+  bucket = var.name
+
+  # block delete unless the bucket is empty
+  force_destroy = false
+
+  tags = var.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+# Disallow public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ terraform/remote-state/versions.tf
@@ -0,0 +1,20 @@
+﻿terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.75.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = var.name
+    key    = "bootstrap"
+    region = var.region
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
