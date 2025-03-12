17318c3a64539beb80b9ff8657d2cdf8a6ec4d58
Nico Hauser <nico@anansi-solutions.net>
2025-03-12 07:15:55 +0100

    [CRT-95] Improve test

--- backend/src/utilities/json-to-object-interceptor.spec.ts
+++ backend/src/utilities/json-to-object-interceptor.spec.ts
@@ -3,6 +3,13 @@ import { CallHandler } from "@nestjs/common";
 import { HttpArgumentsHost } from "@nestjs/common/interfaces";
 import { JsonToObjectsInterceptor } from "./json-to-object-interceptor";
 
+const body = {
+  field1: '{"key":"value"}',
+  field2: '{"key2":"value2"}',
+  field3: "not {valid} [json]",
+  field4: '{"not":"parsed"}',
+};
+
 describe("JsonToObjectsInterceptor", () => {
   let interceptor: NestInterceptor;
   let context: ExecutionContext & HttpArgumentsHost;
@@ -13,12 +20,7 @@ describe("JsonToObjectsInterceptor", () => {
     context = {
       switchToHttp: jest.fn().mockReturnThis(),
       getRequest: jest.fn().mockReturnValue({
-        body: {
-          field1: '{"key":"value"}',
-          field2: '{"key2":"value2"}',
-          field3: "not a json string",
-          field4: '{"not":"parsed"}',
-        },
+        body: { ...body },
       }),
     } as unknown as ExecutionContext & HttpArgumentsHost;
     next = {
@@ -32,10 +34,12 @@ describe("JsonToObjectsInterceptor", () => {
     interceptor.intercept(context, next);
 
     const request = context.switchToHttp().getRequest();
-    expect(request.body.field1).toEqual({ key: "value" });
-    expect(request.body.field2).toEqual({ key2: "value2" });
-    expect(request.body.field3).toEqual("not a json string");
-    expect(request.body.field4).toEqual('{"not":"parsed"}');
+    expect(request.body).toEqual({
+      field1: JSON.parse(body.field1),
+      field2: JSON.parse(body.field2),
+      field3: body.field3,
+      field4: body.field4,
+    });
   });
 
   it("should call next.handle()", () => {
