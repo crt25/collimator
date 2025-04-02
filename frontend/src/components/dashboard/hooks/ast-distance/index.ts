8bc492983333b19a093b63a799c2bceec2e1d28b
Pierluca Borsò <pierluca@anansi-solutions.net>
2025-04-02 17:27:09 +0200

    [CRT-47] Added Zhang-Shasha tree-edit distance

--- frontend/src/components/dashboard/hooks/ast-distance/index.ts
+++ frontend/src/components/dashboard/hooks/ast-distance/index.ts
@@ -2,6 +2,7 @@ import { AstNode } from "@ast/index";
 import { match } from "ts-pattern";
 import { DistanceType } from "./distance-type";
 import { computePqGramsDistance } from "./pqgrams-distance";
+import { computeZhangShashaDistance } from "./zhangshasha-distance";
 
 export const getAstDistance = (
   type: DistanceType,
@@ -11,4 +12,5 @@ export const getAstDistance = (
   match(type)
     .returnType<Promise<number>>()
     .with(DistanceType.pqGrams, () => computePqGramsDistance(a, b))
+    .with(DistanceType.zhangShasha, () => computeZhangShashaDistance(a, b))
     .exhaustive();
