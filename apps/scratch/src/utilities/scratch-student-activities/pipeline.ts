2ce57f5190e7c0075132c3aaf6428bac54049f2e
Burak Kose <burak@anansi-solutions.net>
2025-10-20 18:42:16 +0200

    [CRT-154] Created studentActivity action pipeline

---
+++ apps/scratch/src/utilities/scratch-student-activities/pipeline.ts
@@ -0,0 +1,50 @@
+import {
+  CreatePipelineParams,
+  DeletePipelineParams,
+  MovePipelineParams,
+  StudentAction,
+} from "../../types/scratch-student-activities";
+
+import {
+  trackCreateActivity,
+  trackMoveActivity,
+  trackDeleteActivity,
+} from "./index";
+
+type StudentActivityPipelineParams =
+  | CreatePipelineParams
+  | MovePipelineParams
+  | DeletePipelineParams;
+
+export const processStudentActivityPipeline = (
+  params: StudentActivityPipelineParams,
+): void => {
+  switch (params.action) {
+    case StudentAction.Create:
+      trackCreateActivity({
+        block: params.block,
+        sendRequest: params.sendRequest,
+        solution: params.solution,
+        event: params.event,
+      });
+      break;
+
+    case StudentAction.Move:
+      trackMoveActivity({
+        block: params.block,
+        sendRequest: params.sendRequest,
+        solution: params.solution,
+        event: params.event,
+      });
+      break;
+
+    case StudentAction.Delete:
+      trackDeleteActivity({
+        block: params.block,
+        sendRequest: params.sendRequest,
+        solution: params.solution,
+        event: params.event,
+      });
+      break;
+  }
+};
