d66e0c7341fed1242cd71ef42c8c16b176a5e7e7
Burak Kose <burak@anansi-solutions.net>
2025-10-20 18:18:17 +0200

    [CRT-154] Created activity base types

---
+++ apps/scratch/src/types/scratch-student-activities/common.ts
@@ -0,0 +1,41 @@
+import { Block, Workspace } from "scratch-blocks";
+import { CrtContextValue } from "../../contexts/CrtContext";
+import { WorkspaceChangeEvent } from "../scratch-workspace";
+
+export interface BaseStudentAppActivity extends Record<string, unknown> {
+  blockId: string;
+  blockType: string;
+}
+
+export interface BaseActivityRequest {
+  sendRequest: CrtContextValue["sendRequest"];
+  solution: Blob;
+}
+
+export interface BaseStudentAction {
+  block: Block;
+  sendRequest: CrtContextValue["sendRequest"];
+  solution: Blob;
+  event: WorkspaceChangeEvent;
+}
+
+export enum StudentAction {
+  Create = "create",
+  Move = "move",
+  Delete = "delete",
+}
+
+export interface BasePipelineParams {
+  event: WorkspaceChangeEvent;
+  sendRequest: CrtContextValue["sendRequest"];
+  solution: Blob;
+}
+
+export interface StudentActivityHandlerParams {
+  event: WorkspaceChangeEvent;
+  action: StudentAction;
+  canEditTask: boolean | undefined;
+  sendRequest: CrtContextValue["sendRequest"];
+  solution: Blob;
+  getWorkspace: () => Workspace;
+}
