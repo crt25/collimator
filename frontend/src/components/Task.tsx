c3b6e83e42f8f2406eec22a7995c53c3b6a8885e
Burak Kose <burak@anansi-solutions.net>
2025-10-21 19:52:30 +0200

    [CRT-154] Modified data to generic unknown

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -8,10 +8,10 @@ import { ExistingSessionExtended } from "@/api/collimator/models/sessions/existi
 import { ExistingTask } from "@/api/collimator/models/tasks/existing-task";
 import { useTrackStudentActivity } from "@/api/collimator/hooks/student-activity/useTrackStudentActivity";
 import { StudentActivityType } from "@/api/collimator/generated/models";
-import VerticalSpacing from "./layout/VerticalSpacing";
-import FullHeightRow from "./layout/FullHeightRow";
-import RemainingHeightContainer from "./layout/RemainingHeightContainer";
 import TaskList from "./TaskList";
+import RemainingHeightContainer from "./layout/RemainingHeightContainer";
+import FullHeightRow from "./layout/FullHeightRow";
+import VerticalSpacing from "./layout/VerticalSpacing";
 
 const TaskWrapper = styled.div`
   flex-grow: 1;
@@ -89,7 +89,7 @@ const Task = ({
   );
 
   const onStudentAppActivity = useCallback(
-    (action: string, data: Record<string, unknown>, solution: Blob) => {
+    (action: string, data: unknown, solution: Blob) => {
       trackStudentActivity({
         type: StudentActivityType.TASK_APP_ACTIVITY,
         sessionId: session.id,
