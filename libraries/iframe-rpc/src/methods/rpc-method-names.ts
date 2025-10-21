db503c502b1cf26b3135fc91af67796bc1b16d22
Burak Kose <burak@anansi-solutions.net>
2025-10-21 19:53:39 +0200

    [CRT-154] Removed unused file/method

--- libraries/iframe-rpc/src/methods/rpc-method-names.ts
+++
@@ -1,5 +0,0 @@
-import { IframeRpcApplicationMessage, IframeRpcPlatformMessage } from ".";
-
-export type RpcMethodName =
-  | IframeRpcPlatformMessage["method"]
-  | IframeRpcApplicationMessage["method"];
