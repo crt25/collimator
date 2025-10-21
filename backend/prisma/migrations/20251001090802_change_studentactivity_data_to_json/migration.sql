73b4a185cd04caff5269efdc1f3a642bf8be533d
Burak Kose <burak@anansi-solutions.net>
2025-10-21 17:42:58 +0200

    [CRT-154] Addressed migration comment

--- backend/prisma/migrations/20251001090802_change_studentactivity_data_to_json/migration.sql
+++ backend/prisma/migrations/20251001090802_change_studentactivity_data_to_json/migration.sql
@@ -3,6 +3,10 @@
 
   - Changed the type of `data` on the `StudentActivityApp` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
+  Why this is okay:
+  The application is not yet in production, so there is no production data to worry about.
+  Furthermore, the application is still in active development, so any development data can be discarded. 
+
 */
 -- AlterTable
 ALTER TABLE "StudentActivityApp" DROP COLUMN "data",
