dc44b4c6fb26b6c96561d983c758fc38aba37349
Burak Kose <burak@anansi-solutions.net>
2025-10-21 13:07:16 +0200

    [CRT-154] Added explanation on nullable move field

--- apps/scratch/src/types/scratch-student-activities/move/types.ts
+++ apps/scratch/src/types/scratch-student-activities/move/types.ts
@@ -1,7 +1,10 @@
 import { StudentAppActivity, ActivityRequest, StudentAction } from "../common";
 
 export interface StudentMoveActivity extends StudentAppActivity {
+  // The id of the block's previous parent (null if top-level)
   oldParentId: string | null;
+
+  // The id of the block's new parent (null if top-level)
   newParentId: string | null;
 }
 
