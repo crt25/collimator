cc8e1bbe943939f14e5fd326001bda89a4bcf529
Burak Kose <burak@anansi-solutions.net>
2025-10-22 17:23:02 +0200

    [CRT-154] Added move activity ts doc comment

--- apps/scratch/src/types/scratch-student-activities/move/types.ts
+++ apps/scratch/src/types/scratch-student-activities/move/types.ts
@@ -1,9 +1,9 @@
 import { StudentAppActivity } from "../common";
 
 export interface StudentMoveActivity extends StudentAppActivity {
-  // The id of the block's previous parent (null if top-level)
+  /** The id of the block's previous parent (null if top-level) */
   oldParentId: string | null;
 
-  // The id of the block's new parent (null if top-level)
+  /** The id of the block's new parent (null if top-level) */
   newParentId: string | null;
 }
