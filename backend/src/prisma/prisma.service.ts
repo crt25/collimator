4ea33cf7019c9614bb35710167db6c9b8a72609c
Burak Kose <burak@anansi-solutions.net>
2025-10-22 17:12:32 +0200

    [CRT-37] Added extension on prisma service

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
-
+import { softDeleteExtension } from "src/api/extensions/softDeleteExtension";
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     super();
+    this.$extends(softDeleteExtension);
   }
 
   async onModuleInit(): Promise<void> {
