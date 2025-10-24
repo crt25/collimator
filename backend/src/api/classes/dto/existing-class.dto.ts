56535db7df68ca7fd288d5409ec47a427cfd605c
Burak Kose <burak@anansi-solutions.net>
2025-10-24 10:41:50 +0200

    [CRT-37] Update classes controller/service/dto

--- backend/src/api/classes/dto/existing-class.dto.ts
+++ backend/src/api/classes/dto/existing-class.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Class } from "@prisma/client";
 import { Expose, plainToInstance } from "class-transformer";
+import { IsDate } from "class-validator";
 import { CreateClassDto } from "./create-class.dto";
 
 export type ClassId = number;
@@ -12,6 +13,11 @@ export class ExistingClassDto extends CreateClassDto implements Class {
   })
   @Expose()
   readonly id!: ClassId;
+
+  @IsDate()
+  @ApiProperty({ nullable: true })
+  @Expose()
+  readonly deletedAt!: Date | null;
 
   static fromQueryResult(data: Class): ExistingClassDto {
     return plainToInstance(ExistingClassDto, data, {
