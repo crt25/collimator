4e67f294d04c7e515691a5c258c1319ddf311917
Burak Kose <burak@anansi-solutions.net>
2025-10-24 12:21:20 +0200

    [CRT-159] Add task format enum

---
+++ apps/jupyter/extensions/notebook-runner/src/task-format.ts
@@ -0,0 +1,25 @@
+export enum TaskFormat {
+  CrtInternal = "CrtInternal",
+  ExternalCustom = "ExternalCustom",
+  Unknown = "Unknown",
+}
+
+export const CrtFileIdentifier = ".crt";
+
+export const CrtInternalFiles = {
+  Template: "template.ipynb",
+  Student: "student.ipynb",
+  Autograder: "autograder.zip",
+  Data: "data",
+  GradingData: "grading_data",
+  Src: "src",
+  GradingSrc: "grading_src",
+};
+
+export const ExternalCustomFiles = {
+  Task: "task.ipynb",
+  Data: "data",
+  GradingData: "grading_data",
+  Src: "src",
+  GradingSrc: "grading_src",
+};
