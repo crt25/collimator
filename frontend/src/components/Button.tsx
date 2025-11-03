ca096142cc4d3adea07bac2a4e92cc132fac7e82
Burak Kose <burak@anansi-solutions.net>
2025-11-03 12:40:16 +0100

    [CRT-197] Migrate Button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,18 +1,19 @@
-import styled from "@emotion/styled";
-import {
-  ButtonHTMLAttributes,
-  DetailedHTMLProps,
-  MouseEvent as MouseEventReact,
-  useCallback,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import { Button as ChakraButton, HStack, Icon, Box } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCheckCircle,
   faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  useCallback,
+  useState,
+  useRef,
+  useMemo,
+  MouseEvent as MouseEventReact,
+  DetailedHTMLProps,
+  ButtonHTMLAttributes,
+} from "react";
 import { isNonNull } from "@/utilities/is-non-null";
 import ProgressSpinner from "./ProgressSpinner";
 
@@ -22,134 +23,36 @@ export enum ButtonVariant {
   danger = "danger",
 }
 
-const colorsByVariant: {
-  [key in ButtonVariant]: {
-    background: string;
-    foreground: string;
-  };
-} = {
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  variant?: ButtonVariant;
+  active?: boolean;
+};
+
+const colorsByVariant = {
   [ButtonVariant.primary]: {
-    background: "--button-background-color",
-    foreground: "--button-foreground-color",
+    bgColor: "var(--button-background-color)",
+    fgColor: "var(--button-foreground-color)",
   },
   [ButtonVariant.secondary]: {
-    background: "--button-secondary-background-color",
-    foreground: "--button-secondary-foreground-color",
+    bgColor: "var(--button-secondary-background-color)",
+    fgColor: "var(--button-secondary-foreground-color)",
   },
   [ButtonVariant.danger]: {
-    background: "--button-danger-background-color",
-    foreground: "--button-danger-foreground-color",
+    bgColor: "var(--button-danger-background-color)",
+    fgColor: "var(--button-danger-foreground-color)",
   },
 };
 
-const WrapperComponentByVariant = Object.fromEntries(
-  Object.entries(colorsByVariant).map(([variant, colors]) => [
-    variant,
-    styled.div`
-      --btn-bg-color: var(${colors.background});
-      --btn-bg-color-hover: color-mix(
-        in srgb,
-        var(${colors.background}),
-        #000 15%
-      );
-
-      --btn-fg-color: var(${colors.foreground});
-
-      /* if direct child of a button group with a sibling -> no border radius */
-      &:is(.btn-group > div):has(+ *) button {
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;
-      }
-
-      &:is(.btn-group > div) + * button {
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;
-      }
-    `,
-  ]),
-);
-
-const StyledButton = styled.button`
-  position: relative;
-
-  padding: 0.5rem 1rem;
-  border-radius: var(--border-radius);
-
-  border: none;
-
-  background-color: var(--btn-bg-color);
-  color: var(--btn-fg-color);
-
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  gap: 1rem;
-
-  &.active,
-  &:hover {
-    background-color: var(--btn-bg-color-hover);
-  }
-
-  &:disabled {
-    opacity: 0.65;
-  }
-`;
-
-const ButtonState = styled.div`
-  // the loading spinner / checkmark should never take more than the minimal space
-  flex-grow: 0;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  .icon {
-    background-color: #fff;
-    color: var(--btn-state-color);
-    border-radius: 50%;
-  }
-
-  .p-progress-spinner {
-    width: 1.25rem;
-    height: 1.25rem;
-
-    svg circle {
-      stroke-width: 8;
-      stroke: #fff !important;
-    }
-  }
-`;
-
-const ButtonChildren = styled.div`
-  flex-grow: 1;
-`;
-
-const Success = styled.div`
-  --btn-state-color: var(--success-color);
-`;
-const Failure = styled.div`
-  --btn-state-color: var(--error-color);
-`;
-
-const Button = (
-  props: DetailedHTMLProps<
-    ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > & {
-    variant?: ButtonVariant;
-    active?: boolean;
-  },
-) => {
-  const {
-    onClick: onClickFn,
-    children,
-    variant,
-    active,
-    ...buttonProps
-  } = props;
-
+const Button = ({
+  onClick: onClickFn,
+  children,
+  variant,
+  active,
+  ...buttonProps
+}: ButtonProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccessful, setIsSuccessful] = useState<boolean | null>(null);
   const enabled = useRef(true);
@@ -192,11 +95,6 @@ const Button = (
     [onClickFn, showPromiseResult],
   );
 
-  const WrapperComponent = useMemo(
-    () => WrapperComponentByVariant[variant ?? ButtonVariant.primary],
-    [variant],
-  );
-
   const className = useMemo(
     () =>
       [active ? "active" : null, buttonProps.className ?? null]
@@ -205,35 +103,56 @@ const Button = (
     [buttonProps.className, active],
   );
 
+  const cssProps = colorsByVariant[variant ?? ButtonVariant.primary];
+
+  const { bgColor, fgColor } = cssProps;
+
+  const buttonCssProps = {
+    borderRadius: "var(--border-radius)",
+    backgroundColor: bgColor,
+    color: fgColor,
+    "&:hover": {
+      backgroundColor: "var(--accent-color-highlight)",
+    },
+    "&.active": {
+      backgroundColor: "var(--accent-color-highlight)",
+    },
+    padding: "0.5rem 1rem",
+  };
+
   return (
-    <WrapperComponent>
-      <StyledButton {...buttonProps} className={className} onClick={onClick}>
-        <ButtonChildren>{children}</ButtonChildren>
-        {(isLoading || isSuccessful !== null) && (
-          <ButtonState>
-            {isLoading ? (
-              <ProgressSpinner />
-            ) : isSuccessful === true ? (
-              <Success>
-                <FontAwesomeIcon
-                  icon={faCheckCircle}
-                  className="icon"
-                  data-testid="success-icon"
-                />
-              </Success>
-            ) : isSuccessful === false ? (
-              <Failure>
-                <FontAwesomeIcon
-                  icon={faTimesCircle}
-                  className="icon"
-                  data-testid="failure-icon"
-                />
-              </Failure>
-            ) : null}
-          </ButtonState>
+    <ChakraButton
+      onClick={onClick}
+      loading={isLoading}
+      spinner={<ProgressSpinner />}
+      className={className}
+      css={buttonCssProps}
+      {...buttonProps}
+    >
+      <HStack gap={3}>
+        <Box>{children}</Box>
+        {isSuccessful === true && (
+          <Icon
+            as={() => (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                data-testid="success-icon"
+              />
+            )}
+          />
         )}
-      </StyledButton>
-    </WrapperComponent>
+        {isSuccessful === false && (
+          <Icon
+            as={() => (
+              <FontAwesomeIcon
+                icon={faTimesCircle}
+                data-testid="failure-icon"
+              />
+            )}
+          />
+        )}
+      </HStack>
+    </ChakraButton>
   );
 };
 
