f990cf219ff413d703cdd01c13b808cc891b2c72
Burak Kose <burak@anansi-solutions.net>
2025-11-05 17:23:40 +0100

    [CRT-197] Add document prop comment

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,6 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
+    // Adding the suppressHydrationWarning prop to Html to prevent hydration mismatch warnings
+    // see: https://chakra-ui.com/docs/get-started/frameworks/next-app#setup-provider
     <Html suppressHydrationWarning>
       <Head />
       <body>
