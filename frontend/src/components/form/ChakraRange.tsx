d878d2633ce955467134ae5aeba0cad85552dc2e
Burak Kose <burak@anansi-solutions.net>
2025-11-05 18:24:36 +0100

    [CRT-197] Merge range with ChakraRange

--- frontend/src/components/form/ChakraRange.tsx
+++ frontend/src/components/form/ChakraRange.tsx
@@ -1,10 +1,63 @@
 import styled from "@emotion/styled";
 import { Slider } from "@chakra-ui/react";
 import React from "react";
+import { MessageDescriptor, useIntl } from "react-intl";
 
 const ChakraSliderWrapper = styled.div`
   margin-bottom: 1rem;
 `;
+
+const Label = styled.span`
+  display: block;
+  margin-bottom: 0.25rem;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CurrentValue = styled.span`
+  margin-bottom: 0.25rem;
+  text-align: right;
+`;
+
+const Range = ({
+  value,
+  min,
+  max,
+  step,
+  onChange,
+  label,
+  children,
+}: {
+  value?: number;
+  min: number;
+  max: number;
+  step?: number;
+  onChange: (value: number) => void;
+  label: MessageDescriptor;
+  children: React.ReactNode;
+}) => {
+  const intl = useIntl();
+
+  return (
+    <>
+      <Label>{intl.formatMessage(label)}</Label>
+      <InputWrapper>
+        <CurrentValue>{value}</CurrentValue>
+        <ChakraRange
+          min={min}
+          max={max}
+          step={step ?? 1}
+          value={value === undefined ? undefined : [value]}
+          onChange={(values) => onChange(values[0])}
+        />
+      </InputWrapper>
+      {children}
+    </>
+  );
+};
 
 interface ChakraSliderProps {
   min: number;
@@ -55,3 +108,4 @@ const ChakraRange = ({
 );
 
 export default ChakraRange;
+export { Range };
