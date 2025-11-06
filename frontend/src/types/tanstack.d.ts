5eca3a05cb28beee7c8166d468a9777b53ee0739
Burak Kose <burak@anansi-solutions.net>
2025-11-06 14:21:14 +0100

    [CRT-197] Add columnMeta parameters

--- frontend/src/types/tanstack.d.ts
+++ frontend/src/types/tanstack.d.ts
@@ -1,7 +1,9 @@
 import { RowData } from "@tanstack/react-table";
 import { ColumnType } from "./tanstack-types";
 declare module "@tanstack/react-table" {
-  interface ColumnMeta {
+  // The TValue generic is required by TanStack Table but not used in our extension so we disable the unused vars rule for it.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ColumnMeta<TData extends RowData, TValue> {
     columnType?: ColumnType;
     icon?: React.ReactNode;
     linkHref?: string;
