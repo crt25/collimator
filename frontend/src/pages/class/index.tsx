b43415c120c1fe94eb3247e07706625d81333891
Burak Kose <burak@anansi-solutions.net>
2025-11-07 15:55:07 +0100

    [CRT-168] Update class list index styling

--- frontend/src/pages/class/index.tsx
+++ frontend/src/pages/class/index.tsx
@@ -1,8 +1,9 @@
 import { Container } from "react-bootstrap";
 import { defineMessages, FormattedMessage } from "react-intl";
+import styled from "@emotion/styled";
+import { Heading } from "@chakra-ui/react";
 import ClassList from "@/components/class/ClassList";
 import Header from "@/components/Header";
-import PageHeader from "@/components/PageHeader";
 import CrtNavigation from "@/components/CrtNavigation";
 import Breadcrumbs from "@/components/Breadcrumbs";
 
@@ -13,22 +14,40 @@ const messages = defineMessages({
   },
 });
 
+const StyledContainer = styled(Container)`
+  padding-top: 0;
+  padding-left: 0;
+`;
+
+const PageDescription = styled.p`
+  font-size: 1rem;
+  color: #000000ff;
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+`;
+
 const ListClasses = () => {
   return (
     <>
       <Header title={messages.title} />
-      <Container>
+      <StyledContainer>
         <Breadcrumbs>
           <CrtNavigation breadcrumb />
         </Breadcrumbs>
-        <PageHeader>
+        <Heading mt="10" mb="2">
           <FormattedMessage
-            id="ListClasses.header"
-            defaultMessage="Class Manager"
+            id="ListClasses.pageTitle"
+            defaultMessage="Classes Page Title"
           />
-        </PageHeader>
+        </Heading>
+        <PageDescription>
+          <FormattedMessage
+            id="ListClasses.pageDescription"
+            defaultMessage="Lorem ipsum dolor sit amet consectetur. Aliquam cursus risus augue quis est."
+          />
+        </PageDescription>
         <ClassList />
-      </Container>
+      </StyledContainer>
     </>
   );
 };
