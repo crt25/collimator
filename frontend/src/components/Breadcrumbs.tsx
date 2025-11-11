faa56f7c59949c449f1511263c16244edade2db7
Burak Kose <burak@anansi-solutions.net>
2025-11-11 11:13:14 +0100

    [CRT-186] Refactor breadcrumb styling

--- frontend/src/components/Breadcrumbs.tsx
+++ frontend/src/components/Breadcrumbs.tsx
@@ -1,35 +1,34 @@
-import { Breadcrumb } from "@chakra-ui/react";
+import { Breadcrumb, HStack, Icon } from "@chakra-ui/react";
 import { FormattedMessage } from "react-intl";
 import { Children, Fragment, isValidElement } from "react";
+import { LuHouse } from "react-icons/lu";
 import BreadcrumbItem from "./BreadcrumbItem";
 
-const Breadcrumbs = ({
-  homeHref,
-  children,
-}: {
-  homeHref?: string;
+interface BreadcrumbsProps {
   children?: React.ReactNode;
-}) => (
+}
+
+const Breadcrumbs = ({ children }: BreadcrumbsProps) => (
   <Breadcrumb.Root>
     <Breadcrumb.List>
-      <BreadcrumbItem href={homeHref ?? "/"}>
-        <FormattedMessage id="Breadcrumbs.home" defaultMessage="Home" />
-      </BreadcrumbItem>
-
-      {Children.map(children, (child, index) => {
-        if (!isValidElement(child)) {
-          return null;
-        }
-
-        const childKey = child.key ?? `breadcrumb-${index}`;
-
-        return (
-          <Fragment key={childKey}>
-            <Breadcrumb.Separator />
-            {child}
-          </Fragment>
-        );
-      })}
+      <HStack>
+        <BreadcrumbItem href="/">
+          <Icon as={LuHouse} />
+          <FormattedMessage id="Breadcrumbs.home" defaultMessage="Home" />
+        </BreadcrumbItem>
+        {Children.map(children, (child, index) => {
+          if (!isValidElement(child)) {
+            return null;
+          }
+          const childKey = child.key ?? `breadcrumb-${index}`;
+          return (
+            <Fragment key={childKey}>
+              <Breadcrumb.Separator />
+              {child}
+            </Fragment>
+          );
+        })}
+      </HStack>
     </Breadcrumb.List>
   </Breadcrumb.Root>
 );
