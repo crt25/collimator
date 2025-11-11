3c098c3226c60900d4584eefb3d5ec8042913470
Burak Kose <burak@anansi-solutions.net>
2025-11-11 11:12:47 +0100

    [CRT-186] Update breadcrumb to support icons

--- frontend/src/components/CrtNavigation.tsx
+++ frontend/src/components/CrtNavigation.tsx
@@ -1,4 +1,11 @@
 import { defineMessages } from "react-intl";
+import {
+  LuHouse,
+  LuGraduationCap,
+  LuListTodo,
+  LuUsers,
+  LuBook,
+} from "react-icons/lu";
 import { ExistingClass } from "@/api/collimator/models/classes/existing-class";
 import { ExistingUser } from "@/api/collimator/models/users/existing-user";
 import { ExistingClassExtended } from "@/api/collimator/models/classes/existing-class-extended";
@@ -34,15 +41,18 @@ const tabs: NavigationTab[] = [
   {
     url: "/",
     title: (intl) => intl.formatMessage(messages.homeTab),
+    icon: <LuHouse />,
   },
   {
     url: "/user",
     title: (intl) => intl.formatMessage(messages.usersTab),
     isShown: (authContext) => authContext.role === UserRole.admin,
+    icon: <LuUsers />,
   },
   {
     url: "/class",
     title: (intl) => intl.formatMessage(messages.classesTab),
+    icon: <LuGraduationCap />,
   },
   // {
   //   url: "/lesson",
@@ -51,6 +61,7 @@ const tabs: NavigationTab[] = [
   {
     url: "/task",
     title: (intl) => intl.formatMessage(messages.tasksTab),
+    icon: <LuListTodo />,
   },
 ];
 
@@ -68,29 +79,34 @@ const CrtNavigation = ({
   lessonId?: number;
 }) => {
   const lessonName = "Introduction to React";
-
   return (
     <>
       <TabNavigation tabs={tabs} breadcrumb={breadcrumb} />
       {breadcrumb && (
         <>
           {user && (
-            <BreadcrumbItem href={`/user/${user.id}`}>
+            <BreadcrumbItem href={`/user/${user.id}`} icon={<LuUsers />}>
               {user.name ?? user.email}
             </BreadcrumbItem>
           )}
           {klass && (
-            <BreadcrumbItem href={`/class/${klass.id}/detail`}>
+            <BreadcrumbItem
+              href={`/class/${klass.id}/detail`}
+              icon={<LuGraduationCap />}
+            >
               {klass.name}
             </BreadcrumbItem>
           )}
           {task && (
-            <BreadcrumbItem href={`/task/${task.id}/detail`}>
+            <BreadcrumbItem
+              href={`/task/${task.id}/detail`}
+              icon={<LuListTodo />}
+            >
               {task.title}
             </BreadcrumbItem>
           )}
           {lessonId && (
-            <BreadcrumbItem href={`/lesson/${lessonId}`}>
+            <BreadcrumbItem href={`/lesson/${lessonId}`} icon={<LuBook />}>
               {lessonName}
             </BreadcrumbItem>
           )}
