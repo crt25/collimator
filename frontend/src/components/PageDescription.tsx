f7fa23ba5586ea1389ddea9e3d5c88a488beb090
Burak Kose <burak@anansi-solutions.net>
2025-11-12 02:08:04 +0100

    [CRT-168] Add PageDescription component

---
+++ frontend/src/components/PageDescription.tsx
@@ -0,0 +1,11 @@
+import { chakra } from "@chakra-ui/react";
+
+const PageDescription = chakra("p", {
+  base: {
+    fontSize: "2xl",
+    color: "textSecondary",
+    marginTop: "md",
+  },
+});
+
+export default PageDescription;
