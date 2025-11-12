5458ac6f6cc5ee6e2366e3d00d87a05f33c899fd
Burak Kose <burak@anansi-solutions.net>
2025-11-12 02:27:47 +0100

    [CRT-168] Refactor class list styling

--- frontend/src/pages/class/index.tsx
+++ frontend/src/pages/class/index.tsx
@@ -1,50 +1,37 @@
-import { Container } from "react-bootstrap";
 import { defineMessages, FormattedMessage } from "react-intl";
-import styled from "@emotion/styled";
-import { Heading } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import ClassList from "@/components/class/ClassList";
 import Header from "@/components/Header";
 import CrtNavigation from "@/components/CrtNavigation";
 import Breadcrumbs from "@/components/Breadcrumbs";
+import PageHeader from "@/components/PageHeader";
+import PageDescription from "@/components/PageDescription";
 
 const messages = defineMessages({
   header: {
     id: "ListClasses.header",
     defaultMessage: "Classes",
   },
+  description: {
+    id: "ListClasses.description",
+    defaultMessage: "",
+  },
 });
-
-const StyledHeading = styled(Heading)`
-  margin-top: 2rem;
-  margin-bottom: 1rem;
-`;
-
-const StyledContainer = styled(Container)`
-  padding-top: 0;
-  padding-left: 0;
-`;
-
-const PageDescription = styled.p`
-  font-size: 1rem;
-  color: var(--page-description-color);
-  margin-top: 1rem;
-  margin-bottom: 3rem;
-`;
 
 const ListClasses = () => {
   return (
     <>
       <Header title={messages.header} />
-      <StyledContainer>
+      <Container>
         <Breadcrumbs>
           <CrtNavigation breadcrumb />
         </Breadcrumbs>
-        <StyledHeading>
+        <PageHeader>
           <FormattedMessage
             id="ListClasses.pageTitle"
             defaultMessage="Class Manager"
           />
-        </StyledHeading>
+        </PageHeader>
         <PageDescription>
           <FormattedMessage
             id="ListClasses.pageDescription"
@@ -52,7 +39,7 @@ const ListClasses = () => {
           />
         </PageDescription>
         <ClassList />
-      </StyledContainer>
+      </Container>
     </>
   );
 };
