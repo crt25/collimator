f64eddd0e5cc9efe4a5112741557485db3c30153
Burak Kose <burak@anansi-solutions.net>
2025-11-12 02:15:20 +0100

    [CRT-168] Refactor IconButton styling

--- frontend/src/components/IconButton.tsx
+++ frontend/src/components/IconButton.tsx
@@ -1,13 +1,14 @@
-import { IconButton as ChakraIconButton } from "@chakra-ui/react";
-import styled from "@emotion/styled";
+import { IconButton as ChakraIconButton, chakra } from "@chakra-ui/react";
 
-export const IconButton = styled(ChakraIconButton)`
-  background-color: var(--button-background-color);
-  color: var(--button-foreground-color);
-  border-radius: var(--border-radius);
-  padding: 0.5rem 1rem;
+export const IconButton = chakra(ChakraIconButton, {
+  base: {
+    backgroundColor: "buttonBg",
+    color: "buttonFg",
+    borderRadius: "sm !important",
+    padding: "sm",
 
-  &:hover {
-    background-color: var(--accent-color-highlight);
-  }
-`;
+    _hover: {
+      backgroundColor: "accentHighlight",
+    },
+  },
+});
