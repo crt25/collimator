bffe59ce53eeaebaef2ae04ac074d25304f6644a
Burak Kose <burak@anansi-solutions.net>
2025-11-13 11:23:27 +0100

    [CRT-173] Add breadcrumbs and page heading to class creation page

--- frontend/src/pages/class/create.tsx
+++ frontend/src/pages/class/create.tsx
@@ -2,10 +2,13 @@ import { Container } from "react-bootstrap";
 import { defineMessages, FormattedMessage } from "react-intl";
 import { useCallback } from "react";
 import { useRouter } from "next/router";
+import { LuSticker } from "react-icons/lu";
 import ClassForm, { ClassFormValues } from "@/components/class/ClassForm";
 import Header from "@/components/Header";
 import CrtNavigation from "@/components/CrtNavigation";
 import { useCreateClass } from "@/api/collimator/hooks/classes/useCreateClass";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import PageHeading, { PageHeadingVariant } from "@/components/PageHeading";
 
 const messages = defineMessages({
   title: {
@@ -33,6 +36,15 @@ const CreateClass = () => {
     },
     [createClass, router],
   );
+
+  const breadcrumbItems = [
+    {
+      label: "Create Class",
+      children: "Create Class",
+      href: "/class/create",
+      icon: <LuSticker />,
+    },
+  ];
 
   return (
     <>
