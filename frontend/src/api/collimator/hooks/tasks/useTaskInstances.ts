09d04f5b0dce7e3ee28103406ab3326177df9323
Nico Hauser <nico@anansi-solutions.net>
2025-11-17 23:24:45 +0100

    [CRT-185] Add new (mock) useTaskInstances hook

---
+++ frontend/src/api/collimator/hooks/tasks/useTaskInstances.ts
@@ -0,0 +1,32 @@
+import useSWR from "swr";
+import { ApiResponse, fromDtos } from "../helpers";
+import {
+  getTasksControllerFindAllV0Url,
+  tasksControllerFindAllV0,
+} from "../../generated/endpoints/tasks/tasks";
+import { ExistingTask } from "../../models/tasks/existing-task";
+import { useAuthenticationOptions } from "../authentication/useAuthenticationOptions";
+import { ExistingSessionExtended } from "../../models/sessions/existing-session-extended";
+
+export type GetTasksReturnType = ExistingTask[];
+
+const fetchAndTransform = (options: RequestInit): Promise<GetTasksReturnType> =>
+  tasksControllerFindAllV0(options).then((data) =>
+    fromDtos(ExistingTask, data),
+  );
+
+export const useTaskInstances = (
+  session: ExistingSessionExtended,
+): ApiResponse<GetTasksReturnType, Error> => {
+  const authOptions = useAuthenticationOptions();
+  const sessionTaskIds = new Set(session.tasks.map((task) => task.id));
+
+  // use the URL with the params as the first entry in the key for easier invalidation
+  return useSWR(
+    [getTasksControllerFindAllV0Url(), "filteredBySessionId", session.id],
+    () =>
+      fetchAndTransform(authOptions).then((tasks) =>
+        tasks.filter((t) => sessionTaskIds.has(t.id)),
+      ),
+  );
+};
