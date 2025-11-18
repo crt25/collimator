93637bfb8b53492a7deb152170c3a7793ab80852
Nico Hauser <nico@anansi-solutions.net>
2025-11-18 11:05:43 +0100

    [CRT-187] Add page to create task instance

---
+++ frontend/src/pages/class/[classId]/session/[sessionId]/task/create.tsx
@@ -0,0 +1,88 @@
+import { useCallback } from "react";
+import { Container } from "@chakra-ui/react";
+import { defineMessages, FormattedMessage } from "react-intl";
+import { useRouter } from "next/router";
+import { useCreateTask } from "@/api/collimator/hooks/tasks/useCreateTask";
+import Header from "@/components/Header";
+import CrtNavigation from "@/components/CrtNavigation";
+import TaskForm, { TaskFormSubmission } from "@/components/task/TaskForm";
+import PageHeading from "@/components/PageHeading";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import { useClass } from "@/api/collimator/hooks/classes/useClass";
+import { useClassSession } from "@/api/collimator/hooks/sessions/useClassSession";
+import ClassNavigation from "@/components/class/ClassNavigation";
+import MultiSwrContent from "@/components/MultiSwrContent";
+import SessionNavigation from "@/components/session/SessionNavigation";
+
+const messages = defineMessages({
+  title: {
+    id: "CreateTaskInstance.title",
+    defaultMessage: "Create Task",
+  },
+  submit: {
+    id: "CreateTaskInstance.submit",
+    defaultMessage: "Create Task",
+  },
+});
+
+const CreateTaskInstance = () => {
+  const router = useRouter();
+  const { classId, sessionId } = router.query as {
+    classId: string;
+    sessionId: string;
+  };
+
+  const {
+    data: klass,
+    error: klassError,
+    isLoading: isLoadingKlass,
+  } = useClass(classId);
+
+  const {
+    data: session,
+    error: sessionError,
+    isLoading: isLoadingSession,
+  } = useClassSession(classId, sessionId);
+
+  const createTask = useCreateTask();
+
+  const onSubmit = useCallback(
+    async (taskSubmission: TaskFormSubmission) => {
+      await createTask(taskSubmission);
+    },
+    [createTask],
+  );
+
+  return (
+    <>
+      <Header title={messages.title} />
+      <Container>
+        <Breadcrumbs>
+          <CrtNavigation breadcrumb klass={klass} />
+          <ClassNavigation classId={klass?.id} breadcrumb session={session} />
+        </Breadcrumbs>
+
+        <MultiSwrContent
+          errors={[klassError, sessionError]}
+          isLoading={[isLoadingKlass, isLoadingSession]}
+          data={[klass, session]}
+        >
+          {([klass, session]) => (
+            <>
+              <PageHeading>
+                <FormattedMessage
+                  id="CreateTaskInstance.header"
+                  defaultMessage="Create Task"
+                />
+              </PageHeading>
+              <SessionNavigation classId={klass.id} sessionId={session.id} />
+              <TaskForm submitMessage={messages.submit} onSubmit={onSubmit} />
+            </>
+          )}
+        </MultiSwrContent>
+      </Container>
+    </>
+  );
+};
+
+export default CreateTaskInstance;
