22de4da529884c1727e3c4c33863dd0c04b2e370
Burak Kose <burak@anansi-solutions.net>
2025-11-18 12:09:01 +0100

    [CRT-180] Add session detail page

---
+++ frontend/src/pages/class/[classId]/session/[sessionId]/detail.tsx
@@ -0,0 +1,149 @@
+import { useRouter } from "next/router";
+import { Container } from "@chakra-ui/react";
+import { defineMessages, useIntl } from "react-intl";
+import { useCallback, useRef } from "react";
+import { UseFormReset } from "react-hook-form";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import ClassNavigation from "@/components/class/ClassNavigation";
+import Header from "@/components/Header";
+import PageHeading from "@/components/PageHeading";
+import CrtNavigation from "@/components/CrtNavigation";
+import { useClass } from "@/api/collimator/hooks/classes/useClass";
+import { useClassSession } from "@/api/collimator/hooks/sessions/useClassSession";
+import { useUpdateClassSession } from "@/api/collimator/hooks/sessions/useUpdateClassSession";
+import SessionNavigation from "@/components/session/SessionNavigation";
+import MultiSwrContent from "@/components/MultiSwrContent";
+import SessionForm, {
+  SessionFormValues,
+} from "@/components/session/SessionForm";
+import { toaster } from "@/components/Toaster";
+import SessionActions from "@/components/session/SessionActions";
+
+const messages = defineMessages({
+  title: {
+    id: "SessionDetail.title",
+    defaultMessage: "Session - {title}",
+  },
+  submit: {
+    id: "SessionDetail.submit",
+    defaultMessage: "Save Session",
+  },
+  successMessage: {
+    id: "SessionDetail.successMessage",
+    defaultMessage: "Successfully saved changes",
+  },
+  errorMessage: {
+    id: "SessionDetail.errorMessage",
+    defaultMessage:
+      "There was an error saving the changes. Please try to save again!",
+  },
+});
+
+const SessionDetail = () => {
+  const intl = useIntl();
+  const router = useRouter();
+  const { classId, sessionId } = router.query as {
+    classId: string;
+    sessionId: string;
+  };
+
+  const {
+    data: klass,
+    error: klassError,
+    isLoading: isLoadingKlass,
+  } = useClass(classId);
+
+  const {
+    data: session,
+    error: sessionError,
+    isLoading: isLoadingSession,
+  } = useClassSession(classId, sessionId);
+
+  const updateSession = useUpdateClassSession();
+  const formResetRef = useRef<UseFormReset<SessionFormValues> | null>(null);
+
+  const onSubmit = useCallback(
+    async (formValues: SessionFormValues) => {
+      if (klass && session) {
+        try {
+          await updateSession(klass.id, session.id, {
+            title: formValues.title,
+            description: formValues.description,
+            taskIds: formValues.taskIds,
+            isAnonymous: formValues.sharingType === "anonymous",
+          });
+          toaster.success({
+            title: intl.formatMessage(messages.successMessage),
+          });
+          if (formResetRef.current) {
+            formResetRef.current(formValues);
+          }
+        } catch {
+          toaster.error({
+            title: intl.formatMessage(messages.errorMessage),
+          });
+        }
+      }
+    },
+    [intl, klass, session, updateSession],
+  );
+
+  const handleFormReady = useCallback(
+    (reset: UseFormReset<SessionFormValues>) => {
+      formResetRef.current = reset;
+    },
+    [],
+  );
+
+  return (
+    <>
+      <Header
+        title={messages.title}
+        titleParameters={{
+          title: session?.title ?? "",
+        }}
+      />
+      <Container>
+        <Breadcrumbs>
+          <CrtNavigation breadcrumb klass={klass} />
+          <ClassNavigation breadcrumb classId={klass?.id} session={session} />
+        </Breadcrumbs>
+        <MultiSwrContent
+          data={[klass, session]}
+          errors={[klassError, sessionError]}
+          isLoading={[isLoadingKlass, isLoadingSession]}
+        >
+          {([
+            /* wait for class to load, needed for update*/
+            _klass,
+            session,
+          ]) => (
+            <>
+              <PageHeading
+                actions={
+                  <SessionActions klass={_klass} sessionId={session.id} />
+                }
+              >
+                {session.title}
+              </PageHeading>
+              <SessionNavigation classId={klass?.id} sessionId={session?.id} />
+              <SessionForm
+                submitMessage={messages.submit}
+                initialValues={{
+                  title: session.title,
+                  description: session.description,
+                  taskIds: session.tasks.map((t) => t.id),
+                  sharingType: session.isAnonymous ? "anonymous" : "public",
+                }}
+                onSubmit={onSubmit}
+                onFormReady={handleFormReady}
+              />
+            </>
+          )}
+        </MultiSwrContent>
+      </Container>
+    </>
+  );
+};
+
+export default SessionDetail;
