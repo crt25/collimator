fb4fba4272be0e5c47e47449bb0806c7db74aafe
Burak Kose <burak@anansi-solutions.net>
2025-11-19 15:32:11 +0100

    [CRT-213] Add shareModal component

---
+++ frontend/src/components/form/ShareModal.tsx
@@ -0,0 +1,78 @@
+import {
+  Dialog,
+  Icon,
+  InputGroup,
+  Input,
+  Clipboard,
+  IconButton,
+  Portal,
+  Text,
+} from "@chakra-ui/react";
+import { JSX } from "react";
+import { IoMdClose } from "react-icons/io";
+import { LuClipboardCopy } from "react-icons/lu";
+
+interface ShareModalProps {
+  title: JSX.Element;
+  subtitle: JSX.Element;
+  description: React.ReactNode;
+  warningText?: string;
+  confirmButtonText: JSX.Element;
+  cancelButtonText: JSX.Element;
+  open: boolean;
+  onOpenChange: (details: { open: boolean }) => void;
+  shareLink?: string;
+}
+
+export const ShareModal = ({
+  title,
+  description,
+  open,
+  subtitle,
+  onOpenChange,
+  shareLink,
+}: ShareModalProps) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange} placement="center">
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>{title}</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body>
+              <Dialog.Description fontWeight="semibold">
+                {subtitle}
+              </Dialog.Description>
+              <Clipboard.Root value={shareLink} marginTop="md">
+                <InputGroup
+                  endElement={
+                    <Clipboard.Trigger asChild>
+                      <IconButton variant="surface" size="xs" marginEnd="-2">
+                        <Clipboard.Indicator asChild>
+                          <LuClipboardCopy />
+                        </Clipboard.Indicator>
+                      </IconButton>
+                    </Clipboard.Trigger>
+                  }
+                  marginBottom="md"
+                >
+                  <Clipboard.Input asChild>
+                    <Input readOnly />
+                  </Clipboard.Input>
+                </InputGroup>
+              </Clipboard.Root>
+              <Text color="gray.500">{description}</Text>
+            </Dialog.Body>
+            <Dialog.CloseTrigger>
+              <Icon>
+                <IoMdClose />
+              </Icon>
+            </Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+};
