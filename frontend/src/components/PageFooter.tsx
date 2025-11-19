2f9c008af4311364137825d4614b0a72e96c7963
Loïc Aubrays <contact@aubrays.ch>
2025-11-19 17:00:23 +0100

    add internalization support

--- frontend/src/components/PageFooter.tsx
+++ frontend/src/components/PageFooter.tsx
@@ -1,33 +1,40 @@
 import { Box, Container, Link, Stack } from "@chakra-ui/react";
 import { Fragment } from "react";
 
-// TODO
-//import { defineMessages } from "react-intl";
-// const messages = defineMessages({
-//   hepLink: {
-//     id: "FooterLink.hepLink",
-//     defaultMessage: "HEP Vaud",
-//   },
-// });
+import { defineMessages, IntlShape, useIntl } from "react-intl";
+
+const messages = defineMessages({
+  hepLink: {
+    id: "FooterLink.hepLink",
+    defaultMessage: "HEP Vaud",
+  },
+  termsLink: {
+    id: "FooterLink.termsLink",
+    defaultMessage: "Terms of Use",
+  },
+  impressumLink: {
+    id: "FooterLink.impressumLink",
+    defaultMessage: "Impressum",
+  },
+});
 
 type FooterLink = {
   url: string;
-  title: string;
+  title: (intl: IntlShape, args: T) => string;
 };
 
 const links: FooterLink[] = [
   {
     url: "#",
-    title: "Impressum",
+    title: (intl) => intl.formatMessage(messages.impressumLink),
   },
   {
     url: "https://www.hepl.ch/accueil/mentions/mentions-legales.html",
-    title: "Terms of Use",
+    title: (intl) => intl.formatMessage(messages.termsLink),
   },
   {
     url: "https://www.hepl.ch",
-    // title: (intl) => intl.formatMessage(messages.hepLink),
-    title: "HEP Vaud",
+    title: (intl) => intl.formatMessage(messages.hepLink),
   },
 ];
 
@@ -40,6 +47,8 @@ const FooterRoot = ({ children }: { children: React.ReactNode }) => {
 };
 
 const FooterList = () => {
+  const intl = useIntl();
+
   return (
     <Stack
       direction={{ smDown: "column", md: "row" }}
@@ -48,13 +57,17 @@ const FooterList = () => {
       alignContent={"center"}
       height={"100%"}
     >
-      {links.map((item, index) => (
-        <Fragment key={index}>
-          <Link href={item.url} variant="plain" fontWeight="bold">
-            {item.title}
-          </Link>
-        </Fragment>
-      ))}
+      {links.map((item, index) => {
+        const title = item.title(intl);
+
+        return (
+          <Fragment key={index}>
+            <Link href={item.url} variant="plain" fontWeight="bold">
+              {title}
+            </Link>
+          </Fragment>
+        );
+      })}
     </Stack>
   );
 };
