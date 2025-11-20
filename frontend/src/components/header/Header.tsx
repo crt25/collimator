abf6aa85252f4f13c274b58508783c19494b63dd
Nico Hauser <nico@anansi-solutions.net>
2025-11-20 10:32:47 +0100

    [CRT-220] Extract header menu

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,18 +1,13 @@
-import Link from "next/link";
 import React from "react";
 import {
   defineMessages,
-  FormattedMessage,
   MessageDescriptor,
   PrimitiveType,
   useIntl,
 } from "react-intl";
 import Head from "next/head";
-import { FaRegUser } from "react-icons/fa6";
 import { chakra, Container } from "@chakra-ui/react";
-import AvatarMenu from "../Avatar";
-import LanguageChooser from "../LanguageChooser";
-import { useIsAuthenticated } from "@/hooks/useIsAuthenticated";
+import HeaderMenu from "./HeaderMenu";
 
 const messages = defineMessages({
   applicationName: {
@@ -55,17 +50,6 @@ const Logo = chakra("div", {
   },
 });
 
-const Menu = chakra("menu", {
-  base: {
-    flexGrow: 1,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    gap: "4xl",
-    listStyle: "none",
-  },
-});
-
 const Header = ({
   title,
   titleParameters,
@@ -79,7 +63,6 @@ const Header = ({
   children?: React.ReactNode;
   hideSignIn?: boolean;
 }) => {
-  const isAuthenticated = useIsAuthenticated();
   const intl = useIntl();
 
   return (
@@ -101,33 +84,7 @@ const Header = ({
         <Container>
           <HeaderInner>
             <Logo>{intl.formatMessage(messages.applicationName)}</Logo>
-            <Menu>
-              {children ?? null}
-              <li>
-                <LanguageChooser />
-              </li>
-              {!hideSignIn && (
-                <li>
-                  {isAuthenticated ? (
-                    <AvatarMenu icon={<FaRegUser />} testId="current-user">
-                      <AvatarMenu.Item href="/logout">
-                        <FormattedMessage
-                          id="Header.signOut"
-                          defaultMessage="Sign Out"
-                        />
-                      </AvatarMenu.Item>
-                    </AvatarMenu>
-                  ) : (
-                    <Link href="/login" data-testid="sign-in-button">
-                      <FormattedMessage
-                        id="Header.signIn"
-                        defaultMessage="Sign In"
-                      />
-                    </Link>
-                  )}
-                </li>
-              )}
-            </Menu>
+            <HeaderMenu hideSignIn={hideSignIn}>{children}</HeaderMenu>
           </HeaderInner>
         </Container>
       </StyledHeader>
