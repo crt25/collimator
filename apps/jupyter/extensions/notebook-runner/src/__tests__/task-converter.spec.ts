652c43520ff75f7422b9a7a9a2a71e2f3575c5d6
Burak Kose <burak@anansi-solutions.net>
2025-12-10 11:33:55 +0100

    [CRT-163] Add import-export cycle tests

--- apps/jupyter/extensions/notebook-runner/src/__tests__/task-converter.spec.ts
+++ apps/jupyter/extensions/notebook-runner/src/__tests__/task-converter.spec.ts
@@ -9,7 +9,11 @@ import {
   assertCrtInternalTaskEquality,
   assertGenericNotebookTaskEquality,
 } from "./helpers/exports";
-import { completeCrtTaskData, completeGenericTaskData } from "./helpers/data";
+import {
+  completeCrtTaskData,
+  emptyCrtTaskData,
+  completeGenericTaskData,
+} from "./helpers/data";
 
 describe("export and import round-trip", () => {
   let cleanupLoad: () => void;
@@ -40,5 +44,53 @@ describe("export and import round-trip", () => {
     const imported = await TaskConverter.importGenericNotebookTask(exported);
 
     await assertGenericNotebookTaskEquality(imported, completeGenericTaskData);
+  });
+
+  it("should be able to import-export-import-export for CRT internal task", async () => {
+    const firstImport = await TaskConverter.importCrtInternalTask(
+      await TaskConverter.exportCrtInternalTask(completeCrtTaskData),
+    );
+
+    const firstExport = await TaskConverter.exportCrtInternalTask(firstImport);
+
+    const secondImport = await TaskConverter.importCrtInternalTask(firstExport);
+
+    const secondExport =
+      await TaskConverter.exportCrtInternalTask(secondImport);
+
+    await assertCrtInternalTaskEquality(
+      await TaskConverter.importCrtInternalTask(secondExport),
+
+      completeCrtTaskData,
+    );
+  });
+
+  it("should be able to import-export-import-export for generic notebook task", async () => {
+    const firstImport = await TaskConverter.importGenericNotebookTask(
+      await TaskConverter.exportExternalCustomTask(completeGenericTaskData),
+    );
+
+    const firstExport =
+      await TaskConverter.exportExternalCustomTask(firstImport);
+
+    const secondImport =
+      await TaskConverter.importGenericNotebookTask(firstExport);
+
+    const secondExport =
+      await TaskConverter.exportExternalCustomTask(secondImport);
+
+    await assertGenericNotebookTaskEquality(
+      await TaskConverter.importGenericNotebookTask(secondExport),
+      completeGenericTaskData,
+    );
+  });
+
+  it("should be able to handle round-trip with empty optional folders", async () => {
+    const exported =
+      await TaskConverter.exportCrtInternalTask(emptyCrtTaskData);
+
+    const imported = await TaskConverter.importCrtInternalTask(exported);
+
+    await assertCrtInternalTaskEquality(imported, emptyCrtTaskData);
   });
 });
