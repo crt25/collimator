4ea3d69e5fcf7da4ea017f376334fe7d5180dad6
Nico Hauser <nico@anansi-solutions.net>
2025-12-16 17:19:52 +0100

    Add back sentry

--- apps/scratch/src/utilities/constants.ts
+++ apps/scratch/src/utilities/constants.ts
@@ -4,6 +4,7 @@ export const isDevelopment =
   (process.env.NEXT_PUBLIC_DEVELOPMENT as unknown as string | undefined) !==
   undefined;
 
-export const sentryDsn = process.env.REACT_APP_SENTRY_DSN as unknown as string;
+export const sentryDsn = process.env
+  .NEXT_PUBLIC_SENTRY_DSN as unknown as string;
 
 export const defaultMaximumExecutionTimeInMs = 1000 * 30;
