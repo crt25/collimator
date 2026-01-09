c0b8f28b02f6efe613c9562d41f78eb6e2b56d24
Nico Hauser <nico@anansi-solutions.net>
2026-01-09 17:12:56 +0100

    [CRT-277] Cover 127.0.0.1 in development environment detection

--- apps/jupyter/extensions/notebook-runner/src/sentry.ts
+++ apps/jupyter/extensions/notebook-runner/src/sentry.ts
@@ -3,7 +3,9 @@ import { VERSION } from "./version";
 
 export const enableSentry = (): void => {
   // Check if running on localhost, i.e. in a development environment
-  const isProductionEnvironment = window.location.hostname !== "localhost";
+  const isProductionEnvironment =
+    window.location.hostname !== "localhost" &&
+    window.location.hostname !== "127.0.0.1";
 
   Sentry.init({
     dsn: "https://2b691a9ac828880dda066b5be1ae9873@o4508199129382912.ingest.de.sentry.io/4510680604016720",
