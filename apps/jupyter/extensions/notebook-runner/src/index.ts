2b14010dcddcf8cd36fdb8902912c980cc057b0f
Nico Hauser <nico@anansi-solutions.net>
2026-01-09 16:45:24 +0100

    [CRT-277] Improve sentry configuration

--- apps/jupyter/extensions/notebook-runner/src/index.ts
+++ apps/jupyter/extensions/notebook-runner/src/index.ts
@@ -53,19 +53,19 @@ const plugin: JupyterFrontEndPlugin<void> = {
     // Check if running on localhost, i.e. in a development environment
     const isProductionEnvironment = window.location.hostname !== "localhost";
 
-    if (isProductionEnvironment) {
-      Sentry.init({
-        dsn: "https://2b691a9ac828880dda066b5be1ae9873@o4508199129382912.ingest.de.sentry.io/4510680604016720",
+    Sentry.init({
+      dsn: "https://2b691a9ac828880dda066b5be1ae9873@o4508199129382912.ingest.de.sentry.io/4510680604016720",
 
-        // Adds request headers and IP for users, for more info visit:
-        // https://docs.sentry.io/platforms/javascript/configuration/options/#sendDefaultPii
-        sendDefaultPii: true,
+      // Adds request headers and IP for users, for more info visit:
+      // https://docs.sentry.io/platforms/javascript/configuration/options/#sendDefaultPii
+      sendDefaultPii: true,
 
-        // Alternatively, use `process.env.npm_package_version` for a dynamic release version
-        // if your build tool supports it.
-        release: `crt-jupyter@${VERSION}`,
-      });
-    }
+      // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+      // if your build tool supports it.
+      release: `crt-jupyter@${VERSION}`,
+      enabled: isProductionEnvironment,
+      environment: isProductionEnvironment ? "production" : "development",
+    });
 
     console.debug("JupyterLab extension notebook-runner is activated!");
 
