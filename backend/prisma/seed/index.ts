15e9fdd5eac999ef1acbcd1f4e8644965d05d517
Athena <athena@anansi-solutions.net>
2026-01-29 12:42:07 +0100

    [CRT-276] Update seeding script

--- backend/prisma/seed/index.ts
+++ backend/prisma/seed/index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { match } from "ts-pattern";
+import * as yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { seedProduction } from "./production";
 import { seedEndToEndTesting } from "./e2e";
 
@@ -8,18 +10,45 @@ enum SeedingMode {
   e2e = "e2e",
 }
 
+async function parseArgs(): Promise<{ email: string; username: string }> {
+  const args = await yargs(hideBin(process.argv))
+    .option("email", {
+      type: "string",
+      demandOption: true,
+      description: "Admin user email",
+    })
+    .option("username", {
+      type: "string",
+      demandOption: true,
+      description: "Admin username",
+    })
+    .parse();
+
+  return {
+    email: args.email,
+    username: args.username,
+  };
+}
+
 const prisma = new PrismaClient();
 
 async function main(): Promise<void> {
   const mode = (process.env.SEEDING_MODE ??
     SeedingMode.production) as SeedingMode;
+  const frontendHostname = process.env.FRONTEND_HOSTNAME as string;
 
-  const seedFn = match(mode)
-    .with(SeedingMode.production, () => seedProduction)
-    .with(SeedingMode.e2e, () => seedEndToEndTesting)
+  await match(mode)
+    .with(SeedingMode.production, async () => {
+      const options = await parseArgs();
+      return seedProduction(
+        prisma,
+        options.email,
+        options.username,
+        frontendHostname,
+      );
+    })
+    .with(SeedingMode.e2e, () => seedEndToEndTesting(prisma))
     .exhaustive();
-
-  return seedFn(prisma);
 }
 
 main()
