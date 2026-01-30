62c9947180f508e927cc0c1bdf17739c1d58aaa2
Athena <athena@anansi-solutions.net>
2026-01-30 18:18:15 +0100

    [CRT-196] Update user detail page

--- frontend/src/components/user/UserForm.tsx
+++ frontend/src/components/user/UserForm.tsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import { defineMessages, MessageDescriptor } from "react-intl";
+import { defineMessages, MessageDescriptor, useIntl } from "react-intl";
 import { useMemo } from "react";
+import { chakra } from "@chakra-ui/react";
 import { useYupSchema } from "@/hooks/useYupSchema";
 import { useYupResolver } from "@/hooks/useYupResolver";
 import { omitNullValues, PartialNullable } from "@/utilities/type";
@@ -12,6 +13,14 @@ import Input from "../form/Input";
 import SubmitFormButton from "../form/SubmitFormButton";
 import Select from "../form/Select";
 import { EditedBadge } from "../EditedBadge";
+
+const ButtonWrapper = chakra("div", {
+  base: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginTop: "4xl",
+  },
+});
 
 const messages = defineMessages({
   name: {
@@ -26,12 +35,21 @@ const messages = defineMessages({
     id: "UserForm.role",
     defaultMessage: "Role",
   },
+  oidcSub: {
+    id: "UserForm.oidcSub",
+    defaultMessage: "OpenID Connect Identifier",
+  },
+  disabledSaveButtonTooltip: {
+    id: "UserForm.tooltip.disabledSaveButton",
+    defaultMessage: "No changes to save",
+  },
 });
 
 export type UserFormValues = {
   name: string;
   email: string;
   type: UserType;
+  readonly oidcSub?: string;
 };
 
 const UserForm = ({
@@ -43,10 +61,13 @@ const UserForm = ({
   initialValues?: PartialNullable<UserFormValues>;
   onSubmit: (data: UserFormValues) => void;
 }) => {
+  const intl = useIntl();
+
   const schema = useYupSchema({
     name: yup.string().required(),
     email: yup.string().email().required(),
     type: yup.string().oneOf(Object.values(UserType)).required(),
+    oidcSub: yup.string().optional(),
   });
 
   const resolver = useYupResolver(schema);
@@ -69,14 +90,17 @@ const UserForm = ({
     register,
     handleSubmit,
     control,
-    formState: { errors, dirtyFields },
+    formState: { errors, dirtyFields, isDirty, isSubmitting },
   } = useForm<UserFormValues>({
     resolver,
     defaultValues,
   });
 
-  // If the intiialValues are provided, show the EditedBadge for fields that have been modified
+  // If the initialValues are provided, show the EditedBadge for fields that have been modified
   const showEditedBadges = !!initialValues;
+
+  // Disable the button if in edit mode with no changes, or if the form is submitting
+  const isButtonDisabled = (showEditedBadges && !isDirty) || isSubmitting;
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} data-testid="user-form">
@@ -112,7 +136,24 @@ const UserForm = ({
         <ValidationErrorMessage>{errors.type?.message}</ValidationErrorMessage>
       </Select>
 
-      <SubmitFormButton label={submitMessage} />
+      <Input
+        label={messages.oidcSub}
+        value={initialValues?.oidcSub ?? ""}
+        data-testid="oidcSub"
+        disabled
+      />
+
+      <ButtonWrapper>
+        <SubmitFormButton
+          label={submitMessage}
+          disabled={isButtonDisabled}
+          title={
+            isDirty
+              ? undefined
+              : intl.formatMessage(messages.disabledSaveButtonTooltip)
+          }
+        />
+      </ButtonWrapper>
     </form>
   );
 };
