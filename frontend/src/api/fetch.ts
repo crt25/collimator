0c57d6d117d7203287a2f8e694f1055bf6ac3313
Athena <athena@anansi-solutions.net>
2026-02-05 10:11:09 +0100

    [CRT-289] Clear authenticationState from local storage when logging out

--- frontend/src/api/fetch.ts
+++ frontend/src/api/fetch.ts
@@ -1,5 +1,5 @@
-import Router from "next/router";
 import { backendHostName } from "@/utilities/constants";
+import { authenticationStateKey } from "@/pages/_app";
 
 export class ApiError extends Error {
   constructor(
@@ -27,7 +27,9 @@ export const fetchApi = async <T>(
 
   if (response.status === 401) {
     // properly sign out the user
-    Router.push("/logout");
+    window.location.href = "/logout";
+    // clear local storage auth entry
+    localStorage.removeItem(authenticationStateKey);
 
     // never-resolving promise - this way the redirect will happen before
     // the UI knows about the error
