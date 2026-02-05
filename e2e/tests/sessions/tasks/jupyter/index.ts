3b63d906d35eca223ee7502d8d500162947cb579
Burak Kose <burak@anansi-solutions.net>
2026-02-05 18:07:04 +0100

    [CRT-202] Add incorrect jupyter solution

--- e2e/tests/sessions/tasks/jupyter/index.ts
+++ e2e/tests/sessions/tasks/jupyter/index.ts
@@ -6,14 +6,14 @@ import { TaskType } from "@/api/collimator/generated/models";
 
 const currentDirectory = import.meta.dirname;
 const correctSolutionPath = path.join(currentDirectory, "solutions", "correct");
-// const incorrectSolutionPath = path.join(
-//   currentDirectory,
-//   "solutions",
-//   "incorrect",
-// );
+const incorrectSolutionPath = path.join(
+  currentDirectory,
+  "solutions",
+  "incorrect",
+);
 
 const jupyterTaskTemplate: TaskTemplateWithSolutions = {
-  type: TaskType.SCRATCH,
+  type: TaskType.JUPYTER,
   mimeType: {
     template: "application/zip",
     solution: "application/zip",
@@ -21,7 +21,7 @@ const jupyterTaskTemplate: TaskTemplateWithSolutions = {
   template: () => readFile(path.join(currentDirectory, "task.zip")),
   solutions: {
     correct: [() => readFile(path.join(correctSolutionPath, "task.zip"))],
-    incorrect: [],
+    incorrect: [() => readFile(path.join(incorrectSolutionPath, "task.zip"))],
     unknown: [],
   },
 };
