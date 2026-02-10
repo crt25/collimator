2ad61b90c1aa2faf0aeba37df08426462cc6e1e3
Athena <athena@anansi-solutions.net>
2026-02-10 14:38:59 +0100

    [CRT-288] Build backend in Dockerfile rather than in container startup

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -6,9 +6,6 @@ yarn prisma migrate deploy
 # the database must be fully migrated before running prisma:generate
 yarn prisma:generate
 
-# now we can build the application
-yarn build
-
 # and seed the database
 yarn prisma:seed
 
