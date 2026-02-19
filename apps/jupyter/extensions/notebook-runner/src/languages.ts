8a33aa7e66d25c059b58beb9854f5fc5e8ecdffc
Burak Kose <burak@anansi-solutions.net>
2026-02-19 08:33:13 +0100

    [CRT-329] Move languages to a separate file

---
+++ apps/jupyter/extensions/notebook-runner/src/languages.ts
@@ -0,0 +1,9 @@
+export enum JupyterLanguageLocales {
+  en = "en",
+  fr = "fr_FR",
+}
+
+export enum LanguageCommands {
+  en = "jupyterlab-translation:en",
+  fr = "jupyterlab-translation:fr_FR",
+}
--- libraries/iframe-rpc/src/languages.ts
+++ libraries/iframe-rpc/src/languages.ts
@@ -2,13 +2,3 @@ export enum Language {
   en = "en",
   fr = "fr",
 }
-
-export enum JupyterLanguageLocales {
-  en = "en",
-  fr = "fr_FR",
-}
-
-export enum LanguageCommands {
-  en = "jupyterlab-translation:en",
-  fr = "jupyterlab-translation:fr_FR",
-}
