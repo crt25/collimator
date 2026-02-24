127926d68926db9c01da4e6ed46c22ebf62385b0
Burak Kose <burak@anansi-solutions.net>
2026-02-24 09:19:35 +0100

    [CRT-316] Refactor visibility checks for debug commands

--- apps/jupyter/extensions/notebook-runner/src/commands/grade.ts
+++ apps/jupyter/extensions/notebook-runner/src/commands/grade.ts
@@ -1,95 +1,87 @@
-import { JupyterFrontEnd } from "@jupyterlab/application";
-import { INotebookTracker, NotebookPanel } from "@jupyterlab/notebook";
-import { Contents, ContentsManager } from "@jupyterlab/services";
-import { IDocumentManager } from "@jupyterlab/docmanager";
-import { NotebookRunnerState } from "../notebook-runner-state";
-import { executeRunNotebookCommand, runGradingCommand } from "../command";
-import { EmbeddedPythonCallbacks } from "../iframe-api";
-import { OtterGradingResults } from "../grading-results";
-import {
-  executePythonInKernel,
-  writeBinaryToVirtualFilesystem,
-  writeJsonToVirtualFilesystem,
-} from "../utils";
-import { DEBUG_NOTEBOOK_RUNNER } from "../constants";
-import {
-  copyRequiredFoldersToKernel,
-  handleOtterCommandError,
-  kernelPaths,
-} from "./helper";
+import {JupyterFrontEnd} from "@jupyterlab/application";
+import {INotebookTracker, NotebookPanel} from "@jupyterlab/notebook";
+import {Contents, ContentsManager} from "@jupyterlab/services";
+import {IDocumentManager} from "@jupyterlab/docmanager";
+import {NotebookRunnerState} from "../notebook-runner-state";
+import {executeRunNotebookCommand, runGradingCommand} from "../command";
+import {EmbeddedPythonCallbacks} from "../iframe-api";
+import {OtterGradingResults} from "../grading-results";
+import {executePythonInKernel, writeBinaryToVirtualFilesystem, writeJsonToVirtualFilesystem,} from "../utils";
+import {DEBUG_NOTEBOOK_RUNNER} from "../constants";
+import {copyRequiredFoldersToKernel, handleOtterCommandError, kernelPaths,} from "./helper";
 
 const createOnNewNotebookListener =
-  (app: JupyterFrontEnd, state: NotebookRunnerState) =>
-  async (
-    _notebookTracker: INotebookTracker,
-    notebookPanel: NotebookPanel,
-  ): Promise<void> => {
-    // close all other notebooks
-    if (!state.allowNextNotebookInParallel) {
-      const widgets = app.shell.widgets("main");
+    (app: JupyterFrontEnd, state: NotebookRunnerState) =>
+        async (
+            _notebookTracker: INotebookTracker,
+            notebookPanel: NotebookPanel,
+        ): Promise<void> => {
+            // close all other notebooks
+            if (!state.allowNextNotebookInParallel) {
+                const widgets = app.shell.widgets("main");
 
-      for (const widget of widgets) {
-        if (widget instanceof NotebookPanel && widget !== notebookPanel) {
-          console.debug("Closing notebook: ", widget);
-          await widget.context.save();
-          await widget.sessionContext.shutdown();
-          widget.close();
-        }
-      }
-    }
-    state.allowNextNotebookInParallel = false;
-  };
+                for (const widget of widgets) {
+                    if (widget instanceof NotebookPanel && widget !== notebookPanel) {
+                        console.debug("Closing notebook: ", widget);
+                        await widget.context.save();
+                        await widget.sessionContext.shutdown();
+                        widget.close();
+                    }
+                }
+            }
+            state.allowNextNotebookInParallel = false;
+        };
 
 export const registerGradeCommand = (
-  state: NotebookRunnerState,
-  app: JupyterFrontEnd,
-  notebookTracker: INotebookTracker,
-  contentsManager: ContentsManager,
-  documentManager: IDocumentManager,
+    state: NotebookRunnerState,
+    app: JupyterFrontEnd,
+    notebookTracker: INotebookTracker,
+    contentsManager: ContentsManager,
+    documentManager: IDocumentManager,
 ): void => {
-  app.commands.addCommand(runGradingCommand, {
-    label: "Run Grading",
-    isVisible: () => (DEBUG_NOTEBOOK_RUNNER ? true : false),
-    execute: async (): Promise<OtterGradingResults> => {
-      try {
-        console.debug("Saving all open notebooks...");
-        const widgets = app.shell.widgets("main");
+    app.commands.addCommand(runGradingCommand, {
+        label: "Run Grading",
+        isVisible: () => DEBUG_NOTEBOOK_RUNNER,
+        execute: async (): Promise<OtterGradingResults> => {
+            try {
+                console.debug("Saving all open notebooks...");
+                const widgets = app.shell.widgets("main");
 
-        const savePromises: Promise<void>[] = [];
+                const savePromises: Promise<void>[] = [];
 
-        for (const widget of widgets) {
-          if (widget instanceof NotebookPanel) {
-            savePromises.push(widget.context.save());
-          }
-        }
-        await Promise.all(savePromises);
+                for (const widget of widgets) {
+                    if (widget instanceof NotebookPanel) {
+                        savePromises.push(widget.context.save());
+                    }
+                }
+                await Promise.all(savePromises);
 
-        console.debug(`Waiting for otter session kernel to be available`);
-        const kernel = await state.getOtterKernel();
+                console.debug(`Waiting for otter session kernel to be available`);
+                const kernel = await state.getOtterKernel();
 
-        console.debug(`Transferring autograder for grading...`);
+                console.debug(`Transferring autograder for grading...`);
 
-        let autograder: Contents.IModel | null = null;
-        try {
-          autograder = await contentsManager.get(
-            EmbeddedPythonCallbacks.autograderLocation,
-            { content: true },
-          );
-        } catch (error) {
-          throw new Error("Error reading autograder:" + error);
-        }
+                let autograder: Contents.IModel | null = null;
+                try {
+                    autograder = await contentsManager.get(
+                        EmbeddedPythonCallbacks.autograderLocation,
+                        {content: true},
+                    );
+                } catch (error) {
+                    throw new Error("Error reading autograder:" + error);
+                }
 
-        await writeBinaryToVirtualFilesystem(
-          kernel,
-          EmbeddedPythonCallbacks.autograderLocation,
-          autograder.content,
-        );
+                await writeBinaryToVirtualFilesystem(
+                    kernel,
+                    EmbeddedPythonCallbacks.autograderLocation,
+                    autograder.content,
+                );
 
-        console.debug(`Unpack tests from autograder.zip to student/`);
+                console.debug(`Unpack tests from autograder.zip to student/`);
 
-        await executePythonInKernel({
-          kernel,
-          code: `
+                await executePythonInKernel({
+                    kernel,
+                    code: `
 import os
 import zipfile
 
@@ -99,53 +91,53 @@ student_path = "/student"
 with zipfile.ZipFile(autograder_path, 'r') as zip_ref:
   zip_ref.extractall(student_path)
 `,
-        });
+                });
 
-        console.debug(`Executing notebook before submitting it for grading`);
+                console.debug(`Executing notebook before submitting it for grading`);
 
-        await executeRunNotebookCommand(
-          app,
-          state,
-          notebookTracker.currentWidget,
-          contentsManager,
-          documentManager,
-          EmbeddedPythonCallbacks.studentTaskLocation,
-          kernelPaths.results,
-        );
+                await executeRunNotebookCommand(
+                    app,
+                    state,
+                    notebookTracker.currentWidget,
+                    contentsManager,
+                    documentManager,
+                    EmbeddedPythonCallbacks.studentTaskLocation,
+                    kernelPaths.results,
+                );
 
-        // read the notebook that has been executed and autograder
-        let executedNotebook: Contents.IModel | null = null;
-        try {
-          executedNotebook = await contentsManager.get(
-            EmbeddedPythonCallbacks.studentTaskLocation,
-            { content: true },
-          );
-        } catch (error) {
-          throw new Error(
-            `Error reading notebook at ${EmbeddedPythonCallbacks.studentTaskLocation} when running otter grade: ${JSON.stringify(error)}`,
-          );
-        }
+                // read the notebook that has been executed and autograder
+                let executedNotebook: Contents.IModel | null = null;
+                try {
+                    executedNotebook = await contentsManager.get(
+                        EmbeddedPythonCallbacks.studentTaskLocation,
+                        {content: true},
+                    );
+                } catch (error) {
+                    throw new Error(
+                        `Error reading notebook at ${EmbeddedPythonCallbacks.studentTaskLocation} when running otter grade: ${JSON.stringify(error)}`,
+                    );
+                }
 
-        console.debug(
-          `Transfering executed notebook to virtual filesystem to '${EmbeddedPythonCallbacks.studentTaskLocation}'`,
-        );
+                console.debug(
+                    `Transfering executed notebook to virtual filesystem to '${EmbeddedPythonCallbacks.studentTaskLocation}'`,
+                );
 
-        await writeJsonToVirtualFilesystem(
-          kernel,
-          EmbeddedPythonCallbacks.studentTaskLocation,
-          executedNotebook.content,
-        );
+                await writeJsonToVirtualFilesystem(
+                    kernel,
+                    EmbeddedPythonCallbacks.studentTaskLocation,
+                    executedNotebook.content,
+                );
 
-        console.debug(
-          "Running notebook with autograder: ",
-          EmbeddedPythonCallbacks.autograderLocation,
-        );
+                console.debug(
+                    "Running notebook with autograder: ",
+                    EmbeddedPythonCallbacks.autograderLocation,
+                );
 
-        await copyRequiredFoldersToKernel(kernel, contentsManager);
+                await copyRequiredFoldersToKernel(kernel, contentsManager);
 
-        const run = executePythonInKernel({
-          kernel,
-          code: `
+                const run = executePythonInKernel({
+                    kernel,
+                    code: `
 run(
   "${EmbeddedPythonCallbacks.studentTaskLocation}",
   autograder="${EmbeddedPythonCallbacks.autograderLocation}",
@@ -156,34 +148,34 @@ run(
   output_dir="/"
 )
           `,
-        });
+                });
 
-        try {
-          await run;
-        } catch (error) {
-          console.error("Error running notebook:", error);
-        }
+                try {
+                    await run;
+                } catch (error) {
+                    console.error("Error running notebook:", error);
+                }
 
-        console.debug("Retrieving results...");
+                console.debug("Retrieving results...");
 
-        const results =
-          await state.readJsonFromVirtualFilesystem<OtterGradingResults>(
-            kernel,
-            "/results.json",
-          );
+                const results =
+                    await state.readJsonFromVirtualFilesystem<OtterGradingResults>(
+                        kernel,
+                        "/results.json",
+                    );
 
-        console.debug("Grading results:", results);
+                console.debug("Grading results:", results);
 
-        return results;
-      } catch (error) {
-        handleOtterCommandError(error);
+                return results;
+            } catch (error) {
+                handleOtterCommandError(error);
 
-        // Propagate error to caller (usually iframe API)
-        throw error;
-      }
-    },
-  });
+                // Propagate error to caller (usually iframe API)
+                throw error;
+            }
+        },
+    });
 
-  // add event listener to run when a notebook is opened
-  notebookTracker.widgetAdded.connect(createOnNewNotebookListener(app, state));
+    // add event listener to run when a notebook is opened
+    notebookTracker.widgetAdded.connect(createOnNewNotebookListener(app, state));
 };
