cb3b311a61f8cbbd7df20a471321d82e831ade12
Burak Kose <burak@anansi-solutions.net>
2026-02-25 10:48:15 +0100

    [CRT-329] Renamed JupyterLanguageLocales enum

--- apps/jupyter/extensions/notebook-runner/src/languages.ts
+++ apps/jupyter/extensions/notebook-runner/src/languages.ts
@@ -1,25 +1,25 @@
 import { Language } from "./iframe-rpc/src";
 
-export enum JupyterLanguageLocales {
+export enum JupyterLanguageLocale {
   en = "en",
   fr = "fr_FR",
   // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
   default = "en",
 }
 
-export const crtToJupyter = new Map<Language, JupyterLanguageLocales>([
-  [Language.en, JupyterLanguageLocales.en],
-  [Language.fr, JupyterLanguageLocales.fr],
+export const crtToJupyter = new Map<Language, JupyterLanguageLocale>([
+  [Language.en, JupyterLanguageLocale.en],
+  [Language.fr, JupyterLanguageLocale.fr],
 ]);
 
 export const jupyterToCrt = new Map<string, Language>([
-  [JupyterLanguageLocales.fr, Language.fr],
-  [JupyterLanguageLocales.en, Language.en],
-  [JupyterLanguageLocales.default, Language.en],
+  [JupyterLanguageLocale.fr, Language.fr],
+  [JupyterLanguageLocale.en, Language.en],
+  [JupyterLanguageLocale.default, Language.en],
 ]);
 
-export const toJupyterLocale = (locale: Language): JupyterLanguageLocales => {
-  return crtToJupyter.get(locale) ?? JupyterLanguageLocales.en;
+export const toJupyterLocale = (locale: Language): JupyterLanguageLocale => {
+  return crtToJupyter.get(locale) ?? JupyterLanguageLocale.en;
 };
 
 export const toCrtLocale = (locale: string): Language => {
