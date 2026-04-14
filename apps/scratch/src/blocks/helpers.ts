9901d338bfc7faf2f485307baafe1439d168b866
Burak Kose <burak@anansi-solutions.net>
2026-04-14 14:32:09 +0200

    [CRT-388] Look up block limit by opcode

--- apps/scratch/src/blocks/helpers.ts
+++ apps/scratch/src/blocks/helpers.ts
@@ -37,7 +37,13 @@ export const shouldPreventBlockCreation = (
     return false;
   }
 
-  if (!wouldExceedLimits(vm)) {
+  const block = workspace.getBlockById(event.blockId || "");
+
+  if (!block) {
+    return false;
+  }
+
+  if (!wouldExceedLimits(vm, block)) {
     return false;
   }
 
@@ -54,7 +60,10 @@ export const shouldPreventBlockCreation = (
  * Check if adding blocks from the flyout would exceed the limits set in the config.
  * It checks the number of blocks currently used in the workspace and the number of blocks being added, and compares it to the limits set in the config.
  */
-export const wouldExceedLimits = (vm: VMExtended): boolean => {
+export const wouldExceedLimits = (
+  vm: VMExtended,
+  block: ScratchBlocksExtended.Block,
+): boolean => {
   const config = vm.crtConfig;
   if (!config) {
     console.debug("No config found, not blocking any blocks");
@@ -66,30 +75,29 @@ export const wouldExceedLimits = (vm: VMExtended): boolean => {
   // using the workspace count would always include the new block, this causes the flyout drags to be blocked
   const usedBlocks = countUsedBlocks(vm);
 
-  for (const [opcode, count] of Object.entries(usedBlocks)) {
-    const allowed = config.allowedBlocks[opcode];
+  const allowed = config.allowedBlocks[block.type];
+  const count = usedBlocks[block.type];
 
-    const isBlockedEntirely = allowed === 0 || allowed === false;
+  const isPreventedEntirely = allowed === 0 || allowed === false;
 
-    if (isBlockedEntirely) {
-      console.debug(
-        `Block ${opcode} is not allowed, blocking addition of blocks`,
-      );
-      return true;
-    }
+  if (isPreventedEntirely) {
+    console.debug(
+      `Block ${block.type} is not allowed, blocking addition of blocks`,
+    );
+    return true;
+  }
 
-    // if allowed is a number, the block has a limit, it doesn't for every other type
-    const hasLimit = typeof allowed === "number";
+  // if allowed is a number, the block has a limit, it doesn't for every other type
+  const hasLimit = typeof allowed === "number";
 
-    // the create event fires on the main workspace before vm.blockListener updates target.blocks._blocks, so
-    // countUsedBlocks does not yet include the new block.
-    // we use >= to prevent going over the limit by one.
-    if (hasLimit && count >= allowed) {
-      console.debug(
-        `Block limit reached for ${opcode}: ${count} >= ${allowed}`,
-      );
-      return true;
-    }
+  // the create event fires on the main workspace before vm.blockListener updates target.blocks._blocks, so
+  // countUsedBlocks does not yet include the new block.
+  // we use >= to prevent going over the limit by one.
+  if (hasLimit && count >= allowed) {
+    console.debug(
+      `Block limit reached for ${block.type}: ${count} >= ${allowed}`,
+    );
+    return true;
   }
 
   return false;
