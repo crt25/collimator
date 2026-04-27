28d0d6a733cd68fb5a0870c09892cd2b92a92e2f
Burak Kose <burak@anansi-solutions.net>
2026-04-27 11:18:27 +0200

    [CRT-356] Avoid fasly values to be preserved

--- apps/scratch/scripts/fill-locales.mjs
+++ apps/scratch/scripts/fill-locales.mjs
@@ -50,7 +50,7 @@ for (const locale of locales) {
     ...deprecatedMessages,
     ...defaultMessagesKeys.map((key) => [
       key,
-      messages[key] || defaultMessages[key],
+      messages[key] ?? defaultMessages[key],
     ]),
   ]);
 
