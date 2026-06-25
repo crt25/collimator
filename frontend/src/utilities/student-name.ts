9ebd8bf1f0396eb90abd4556e05d105e709894d4
bubudetp <burak@anansi-solutions.net>
2026-06-25 15:30:16 +0200

    [CRT-412] Generate nicknames with CRT
    https://cp-algorithms.com/algebra/chinese-remainder-theorem.html

--- frontend/src/utilities/student-name.ts
+++ frontend/src/utilities/student-name.ts
@@ -1,4 +1,3 @@
-import { uniqueNamesGenerator } from "unique-names-generator";
 import { Language } from "iframe-rpc-react/src";
 import animals from "../../content/dictionaries/animals";
 import numbers from "../../content/dictionaries/numbers";
@@ -11,14 +10,12 @@ export function getStudentNickname(
   // We would prefer prefixing the pseudonym to avoid the two name spaces
   // from clashing.
   // However for backwards compatibility we don't, see https://github.com/crt25/collimator/pull/286/files#r2054086764.
-  const seed = pseudonym ? pseudonym : `anonymous_${studentId}`;
-  const animalNames = animals.map((animal) => animal[locale]);
-
-  return uniqueNamesGenerator({
-    dictionaries: [animalNames, numbers],
-    separator: "-",
-    length: 2,
-    style: "lowerCase",
-    seed,
-  });
+  return pseudonym ? pseudonym : generateAnonymousName(studentId, locale);
 }
+
+const generateAnonymousName = (studentId: number, locale: Language): string => {
+  const animalIndex = studentId % animals.length;
+  const numberIndex = studentId % numbers.length;
+
+  return `${animals[animalIndex][locale]}-${numbers[numberIndex]}`;
+};
